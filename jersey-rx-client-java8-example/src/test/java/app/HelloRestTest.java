90d604998880ee9e8c524126313b5c18fe0803ab
Taichi Uragami <backpaper0@gmail.com>
2015-06-25 22:00:59 +0900

    RxJavaのアレも足してみた

--- jersey-rx-client-java8-example/src/test/java/app/HelloRestTest.java
+++ jersey-rx-client-java8-example/src/test/java/app/HelloRestTest.java
@@ -1,29 +1,45 @@
 package app;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.concurrent.CompletionStage;
 
 import javax.ws.rs.core.Application;
 
-import org.glassfish.jersey.client.rx.java8.RxCompletionStage;
+import org.glassfish.jersey.client.rx.Rx;
+import org.glassfish.jersey.client.rx.RxClient;
+import org.glassfish.jersey.client.rx.java8.RxCompletionStageInvoker;
+import org.glassfish.jersey.client.rx.rxjava.RxObservableInvoker;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
+
+import rx.Observable;
 
 public class HelloRestTest extends JerseyTest {
 
     @Test
     public void testRxJava8() throws Exception {
-        CompletionStage<String> stage = RxCompletionStage.from(target("hello"))
+        RxClient<RxCompletionStageInvoker> client = Rx.from(getClient(),
+                RxCompletionStageInvoker.class);
+        CompletionStage<String> stage = client.target(getBaseUri())
+                .path("hello").queryParam("name", "world").request().rx()
+                .get(String.class);
+
+        String s = stage.toCompletableFuture().get();
+        assertThat(s, is("Hello, world!"));
+    }
+
+    @Test
+    public void testRxRxJava() throws Exception {
+        RxClient<RxObservableInvoker> client = Rx.from(getClient(),
+                RxObservableInvoker.class);
+        Observable<String> obs = client.target(getBaseUri()).path("hello")
                 .queryParam("name", "world").request().rx().get(String.class);
 
-        CompletionStage<Void> stage2 = stage.thenAccept(s -> assertThat(s,
-                is("Hello, world!")));
-
-        //終了を待つためアレ
-        stage2.toCompletableFuture().get();
+        String s = obs.toBlocking().first();
+        assertThat(s, is("Hello, world!"));
     }
 
     @Test
