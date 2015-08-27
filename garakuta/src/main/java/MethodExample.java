e24ad2c7fc7d6c4d5d14f75f70fb7cf04ddda205
Taichi Uragami <backpaper0@gmail.com>
2015-08-27 21:06:20 +0900

    isBridgeとisSyntheticなメソッド

---
+++ garakuta/src/main/java/MethodExample.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class MethodExample implements IMethodExample {
+
+    public static void main(String[] args) {
+        Stream.of(MethodExample.class, Inner.class)
+                .flatMap(a -> Arrays.stream(a.getDeclaredMethods()))
+                .forEach(m -> {
+                    System.out.println("----------------");
+                    System.out.println(m.toGenericString());
+                    System.out.printf("isBridge   : %s%n", m.isBridge());
+                    System.out.printf("isSynthetic: %s%n", m.isSynthetic());
+                });
+
+        new MethodExample().new Inner().privateMethod();
+    }
+
+    UnaryOperator<Integer> f = i -> i;
+
+    @Override
+    public Integer get() {
+        return 0;
+    }
+
+    class Inner {
+        private void privateMethod() {
+        }
+    }
+}
+
+interface IMethodExample {
+    Number get();
+}
