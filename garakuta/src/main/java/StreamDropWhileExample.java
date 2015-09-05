1f3d44fd982be7a539a8584d483547b15e71c1f3
Taichi Uragami <backpaper0@gmail.com>
2015-09-06 06:51:56 +0900

    takeWhileも実装してみた

--- garakuta/src/main/java/StreamDropWhileExample.java
+++ garakuta/src/main/java/StreamDropWhileExample.java
@@ -40,6 +40,10 @@ public class StreamDropWhileExample {
                 .collect(joining(", "));
 
         System.out.println(result);
+
+        //takeWhiteも実装してみた。
+        System.out.println(takeWhile(Stream.iterate(1, a -> a + 1), a -> a != 6)
+                .map(Objects::toString).limit(5).collect(joining(", ")));
     }
 
     public static <T> Stream<T> dropWhile(Stream<T> stream,
@@ -48,6 +52,14 @@ public class StreamDropWhileExample {
         //要らない分をfilterで取り除いて
         //元の値をmapで取り出す
         ElementSpliterator<T> sp = new ElementSpliterator<>(stream, predicate);
+        return StreamSupport.stream(sp, false).filter(Element::isNecessary)
+                .map(Element::get);
+    }
+
+    public static <T> Stream<T> takeWhile(Stream<T> stream,
+            Predicate<T> predicate) {
+        TakeWhileSpliterator<T> sp = new TakeWhileSpliterator<>(stream,
+                predicate);
         return StreamSupport.stream(sp, false).filter(Element::isNecessary)
                 .map(Element::get);
     }
@@ -88,6 +100,31 @@ public class StreamDropWhileExample {
             if (iterator.hasNext()) {
                 T value = iterator.next();
                 necessary = necessary || (predicate.test(value) == false);
+                action.accept(new Element<>(necessary, value));
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static class TakeWhileSpliterator<T>
+            extends AbstractSpliterator<Element<T>> {
+
+        private final Iterator<T> iterator;
+        private final Predicate<T> predicate;
+        private boolean necessary;
+
+        public TakeWhileSpliterator(Stream<T> stream, Predicate<T> predicate) {
+            super(Long.MAX_VALUE, 0);
+            this.iterator = Spliterators.iterator(stream.spliterator());
+            this.predicate = predicate;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super Element<T>> action) {
+            if (iterator.hasNext()) {
+                T value = iterator.next();
+                necessary = necessary || predicate.test(value);
                 action.accept(new Element<>(necessary, value));
                 return true;
             }
