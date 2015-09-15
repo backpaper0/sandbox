262040ff73ec6bea6fa751e9e044438067309737
Taichi Uragami <backpaper0@gmail.com>
2015-09-15 22:09:03 +0900

    私の作ったオレオレApplicativeはApplyと呼ばれるものだったらしい！！
    https://twitter.com/gakuzzzz/status/643769177514180608
    https://twitter.com/xuwei_k/status/643770599307784193
    https://twitter.com/xuwei_k/status/643771972627775489
    https://twitter.com/gakuzzzz/status/643772146112593920

--- functor-applicative-study/src/main/scala/functor-applicative-study.scala
+++ functor-applicative-study/src/main/scala/functor-applicative-study.scala
@@ -6,15 +6,15 @@ trait Functor[A, F[_]] { self: F[A] =>
   def fmap[B](f: A => B): F[B]
 }
 
-trait Applicative[A, F[_]] { self: F[A] =>
-  def <*>[B](f: F[A => B]): F[B]
+trait Apply[A, F[_]] { self: F[A] =>
+  def ap[B](f: F[A => B]): F[B]
 }
 
-sealed trait Maybe[A] extends Functor[A, Maybe] with Applicative[A, Maybe]
+sealed trait Maybe[A] extends Functor[A, Maybe] with Apply[A, Maybe]
 
 case class Just[A](value: A) extends Maybe[A] {
   def fmap[B](f: A => B): Maybe[B] = Just(f(value))
-  def <*>[B](f: Maybe[A => B]): Maybe[B] = f match {
+  def ap[B](f: Maybe[A => B]): Maybe[B] = f match {
     case Just(g) => Just(g(value))
     case Nothing() => Nothing()
   }
@@ -22,14 +22,14 @@ case class Just[A](value: A) extends Maybe[A] {
 
 case class Nothing[A]() extends Maybe[A] {
   def fmap[B](f: A => B): Maybe[B] = Nothing()
-  def <*>[B](f: Maybe[A => B]): Maybe[B] = Nothing()
+  def ap[B](f: Maybe[A => B]): Maybe[B] = Nothing()
 }
 
-sealed trait List[A] extends Functor[A, List] with Applicative[A, List]
+sealed trait List[A] extends Functor[A, List] with Apply[A, List]
 
 case class Cons[A](head: A, tail: List[A]) extends List[A] {
   def fmap[B](f: A => B): List[B] = Cons(f(head), tail.fmap(f))
-  def <*>[B](f: List[A => B]): List[B] = {
+  def ap[B](f: List[A => B]): List[B] = {
     def inner(x: A, xs: List[A], fs: List[A => B]): List[B] = (xs, fs) match {
       case (Nil(), Cons(h2, Nil())) => Cons(h2(x), Nil())
       case (Nil(), Cons(h2, t2)) => Cons(h2(x), inner(head, tail, t2))
@@ -42,5 +42,5 @@ case class Cons[A](head: A, tail: List[A]) extends List[A] {
 
 case class Nil[A]() extends List[A] {
   def fmap[B](f: A => B): List[B] = Nil()
-  def <*>[B](f: List[A => B]): List[B] = Nil()
+  def ap[B](f: List[A => B]): List[B] = Nil()
 }
