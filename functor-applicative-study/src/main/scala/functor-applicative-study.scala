de1bc9af099a18f72ed85352d87cb39443a6a040
Taichi Uragami <backpaper0@gmail.com>
2015-09-23 06:10:50 +0900

    implicit classとimplicit parameterでアレ

--- functor-applicative-study/src/main/scala/functor-applicative-study.scala
+++ functor-applicative-study/src/main/scala/functor-applicative-study.scala
@@ -6,29 +6,33 @@ trait Functor[F[_]] {
   def map[A, B](a: F[A], f: A => B): F[B]
 }
 
+object Functors {
+  implicit class FunctorWrapper[A, F[_]](val value: F[A]) {
+    def map[B](f: A => B)(implicit functor: Functor[F]): F[B] = functor.map(value, f)
+  }
+  implicit object MaybeFunctor extends Functor[Maybe] {
+    def map[A, B](a: Maybe[A], f: A => B): Maybe[B] = a match {
+      case Just(value) => Just(f(value))
+      case Nothing => Nothing.asInstanceOf[Maybe[B]]
+    }
+  }
+  implicit object ListFunctor extends Functor[List] {
+    def map[A, B](a: List[A], f: A => B): List[B] = a match {
+      case Cons(head, tail) => Cons(f(head), map(tail, f))
+      case Nil => Nil.asInstanceOf[List[B]]
+    }
+  }
+}
+
 sealed trait Maybe[A]
 
 case class Just[A](value: A) extends Maybe[A]
 
 case object Nothing extends Maybe[scala.Nothing]
 
-object MaybeFunctor extends Functor[Maybe] {
-  def map[A, B](a: Maybe[A], f: A => B): Maybe[B] = a match {
-    case Just(value) => Just(f(value))
-    case Nothing => Nothing.asInstanceOf[Maybe[B]]
-  }
-}
-
 sealed trait List[A]
 
 case class Cons[A](head: A, tail: List[A]) extends List[A]
 
 case object Nil extends List[scala.Nothing]
-
-object ListFunctor extends Functor[List] {
-  def map[A, B](a: List[A], f: A => B): List[B] = a match {
-    case Cons(head, tail) => Cons(f(head), map(tail, f))
-    case Nil => Nil.asInstanceOf[List[B]]
-  }
-}
 
