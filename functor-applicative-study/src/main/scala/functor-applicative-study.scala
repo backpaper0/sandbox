118acae1e9bc866f2542620637f90c11cf116844
Taichi Uragami <backpaper0@gmail.com>
2015-09-22 21:29:00 +0900

    いろいろ考えた感じだとFunctor値はFunctorを継承しない方向が良さそう

--- functor-applicative-study/src/main/scala/functor-applicative-study.scala
+++ functor-applicative-study/src/main/scala/functor-applicative-study.scala
@@ -2,45 +2,33 @@ package study
 
 //import scala.language.higherKinds
 
-trait Functor[A, F[_]] { self: F[A] =>
-  def map[B](f: A => B): F[B]
+trait Functor[F[_]] {
+  def map[A, B](a: F[A], f: A => B): F[B]
 }
 
-trait Apply[A, F[_]] extends Functor[A, F] { self: F[A] =>
-  def ap[B](f: F[A => B]): F[B]
-}
+sealed trait Maybe[A]
 
-sealed trait Maybe[A] extends Apply[A, Maybe]
+case class Just[A](value: A) extends Maybe[A]
 
-case class Just[A](value: A) extends Maybe[A] {
-  def map[B](f: A => B): Maybe[B] = Just(f(value))
-  def ap[B](f: Maybe[A => B]): Maybe[B] = f match {
-    case Just(g) => Just(g(value))
-    case Nothing() => Nothing()
+case object Nothing extends Maybe[scala.Nothing]
+
+object MaybeFunctor extends Functor[Maybe] {
+  def map[A, B](a: Maybe[A], f: A => B): Maybe[B] = a match {
+    case Just(value) => Just(f(value))
+    case Nothing => Nothing.asInstanceOf[Maybe[B]]
   }
 }
 
-case class Nothing[A]() extends Maybe[A] {
-  def map[B](f: A => B): Maybe[B] = Nothing()
-  def ap[B](f: Maybe[A => B]): Maybe[B] = Nothing()
-}
+sealed trait List[A]
 
-sealed trait List[A] extends Apply[A, List]
+case class Cons[A](head: A, tail: List[A]) extends List[A]
 
-case class Cons[A](head: A, tail: List[A]) extends List[A] {
-  def map[B](f: A => B): List[B] = Cons(f(head), tail.map(f))
-  def ap[B](f: List[A => B]): List[B] = {
-    def inner(x: A, xs: List[A], fs: List[A => B]): List[B] = (xs, fs) match {
-      case (Nil(), Cons(h2, Nil())) => Cons(h2(x), Nil())
-      case (Nil(), Cons(h2, t2)) => Cons(h2(x), inner(head, tail, t2))
-      case (Cons(h1, t1), Cons(h2, _)) => Cons(h2(x), inner(h1, t1, fs))
-      case _ => Nil()
-    }
-    inner(head, tail, f)
+case object Nil extends List[scala.Nothing]
+
+object ListFunctor extends Functor[List] {
+  def map[A, B](a: List[A], f: A => B): List[B] = a match {
+    case Cons(head, tail) => Cons(f(head), map(tail, f))
+    case Nil => Nil.asInstanceOf[List[B]]
   }
 }
 
-case class Nil[A]() extends List[A] {
-  def map[B](f: A => B): List[B] = Nil()
-  def ap[B](f: List[A => B]): List[B] = Nil()
-}
--- functor-applicative-study/src/test/scala/functor-applicative-study-test.scala
+++ functor-applicative-study/src/test/scala/functor-applicative-study-test.scala
@@ -9,33 +9,33 @@ class FunctorSpec extends FlatSpec with Matchers {
 
   "Just(2) * 4" should "be Just(4)" in {
     val a: Maybe[Int] = Just(2)
-    a.map(_ * 2) should be (Just(4))
+    MaybeFunctor.map(a, (_: Int) * 2) should be (Just(4))
   }
 
   "Nothing * 4" should "be Nothing" in {
-    val a: Maybe[Int] = Nothing()
-    a.map(_ * 2) should be (Nothing())
+    val a: Maybe[Int] = Nothing.asInstanceOf[Maybe[Int]]
+    MaybeFunctor.map(a, (_: Int) * 2) should be (Nothing)
   }
 
   "List(1, 2, 3) * 4" should "be List(2, 4, 6)" in {
-    val a: List[Int] = Cons(1, Cons(2, Cons(3, Nil())))
-    a.map(_ * 2) should be (Cons(2, Cons(4, Cons(6, Nil()))))
+    val a: List[Int] = Cons(1, Cons(2, Cons(3, Nil.asInstanceOf[List[Int]])))
+    ListFunctor.map(a, (_: Int) * 2) should be (Cons(2, Cons(4, Cons(6, Nil.asInstanceOf[List[Int]]))))
   }
 
   "Nil * 4" should "Nil" in {
-    val a: List[Int] = Nil()
-    a.map(_ * 2) should be (Nil())
+    val a: List[Int] = Nil.asInstanceOf[List[Int]]
+    ListFunctor.map(a, (_: Int) * 2) should be (Nil.asInstanceOf[List[Int]])
   }
 
   "Just(2) map id" should "Just(2)" in {
     val a: Maybe[Int] = Just(2)
-    a.map(x => x) should be (a)
+    MaybeFunctor.map(a, (x: Int) => x) should be (a)
   }
 
   "Just(2) map f map g" should "Just(2) map f andThen g" in {
     val a: Maybe[Int] = Just(2)
     val f: Int => Int = x => x * 2
     val g: Int => String = x => "*" + x + "*"
-    a.map(f).map(g) should be (a.map(f andThen g))
+    MaybeFunctor.map(MaybeFunctor.map(a, f), g) should be (MaybeFunctor.map(a, f andThen g))
   }
 }
