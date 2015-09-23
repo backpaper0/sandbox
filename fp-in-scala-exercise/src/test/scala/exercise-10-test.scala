aa125dd70669cd06ed7e7d1d0fddf71eebd95780
Taichi Uragami <backpaper0@gmail.com>
2015-09-23 15:46:09 +0900

    なんとか則はRuleじゃなくてLawっぽい

--- fp-in-scala-exercise/src/test/scala/exercise-10-test.scala
+++ fp-in-scala-exercise/src/test/scala/exercise-10-test.scala
@@ -9,30 +9,30 @@ class MonoidSpec {
 
   @Test
   def testIntAddition() {
-    testMonoidRule(intAddition, 2, 3, 4)
+    testMonoidLaw(intAddition, 2, 3, 4)
   }
 
   @Test
   def testIntMultiplication() {
-    testMonoidRule(intMultiplication, 2, 3, 4)
+    testMonoidLaw(intMultiplication, 2, 3, 4)
   }
 
   @Test
   def testBooeanOr() {
-    testMonoidRule(booleanOr, true, false, true)
+    testMonoidLaw(booleanOr, true, false, true)
   }
 
   @Test
   def testBooleanAnd() {
-    testMonoidRule(booleanAnd, true, false, true)
+    testMonoidLaw(booleanAnd, true, false, true)
   }
 
   @Test
   def testOptionMonoid() {
-    testMonoidRule(optionMonoid(intAddition), Some(2), Some(3), Some(4))
+    testMonoidLaw(optionMonoid(intAddition), Some(2), Some(3), Some(4))
   }
 
-  private def testMonoidRule[A](m: Monoid[A], a1: A, a2: A, a3: A) {
+  private def testMonoidLaw[A](m: Monoid[A], a1: A, a2: A, a3: A) {
     assertTrue(m.op(m.op(a1, a2), a3) == m.op(a1, m.op(a2, a3)))
     List(a1, a2, a3).foreach { a =>
       assertTrue(m.op(a, m.zero) == a)
