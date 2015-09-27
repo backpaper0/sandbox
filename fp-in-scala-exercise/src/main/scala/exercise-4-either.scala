3f9bc42ca092232acec5c4bd4e65b51db97f3c4f
Taichi Uragami <backpaper0@gmail.com>
2015-09-27 16:37:57 +0900

    EXERCISE 4.6 Eitherのメソッドいろいろ

--- fp-in-scala-exercise/src/main/scala/exercise-4-either.scala
+++ fp-in-scala-exercise/src/main/scala/exercise-4-either.scala
@@ -1,10 +1,26 @@
 package exercise4
 
 sealed trait Either[+E, +A] {
-  def map[B](f: A => B): Either[E, B] = ???
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+
+  //EXERCISE 4.6 (P.76)
+
+  def map[B](f: A => B): Either[E, B] = flatMap(a => Right(f(a)))
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(value) => Left(value)
+    case Right(value) => f(value)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(_) => this
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this, b) match {
+    case (Right(a), Right(b)) => Right(f(a, b))
+    case (Left(value), _) => Left(value)
+    case (_, Left(value)) => Left(value)
+  }
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
--- fp-in-scala-exercise/src/test/scala/exercise-4-either-test.scala
+++ fp-in-scala-exercise/src/test/scala/exercise-4-either-test.scala
@@ -4,4 +4,37 @@ import org.junit._
 
 class Exercise4EitherTest {
 
+  @Test def exercise4_6_right_map: Unit = {
+    assert(Right(1).map(_.toString) == Right("1"))
+  }
+  @Test def exercise4_6_left_map: Unit = {
+    assert(Left(1).map(_.toString) == Left(1))
+  }
+  @Test def exercise4_6_right_flatMap_right: Unit = {
+    assert(Right(1).flatMap(r => Right(r.toString)) == Right("1"))
+  }
+  @Test def exercise4_6_right_flatMap_left: Unit = {
+    assert(Right(1).flatMap(_ => Left(0)) == Left(0))
+  }
+  @Test def exercise4_6_left_flatMap: Unit = {
+    assert(Left(1).flatMap(r => Right(r.toString)) == Left(1))
+  }
+  @Test def exercise4_6_right_orElse: Unit = {
+    assert(Right(1).orElse(Right(2)) == Right(1))
+  }
+  @Test def exercise4_6_left_orElse: Unit = {
+    assert(Left(1).orElse(Right(2)) == Right(2))
+  }
+  @Test def exercise4_6_right_map2_right: Unit = {
+    assert(Right(1).map2(Right(2))(_ + _) == Right(3))
+  }
+  @Test def exercise4_6_left_map2_right: Unit = {
+    assert((Left(1):Either[Int, Int]).map2(Right(2))(_ + _) == Left(1))
+  }
+  @Test def exercise4_6_right_map2_left: Unit = {
+    assert(Right(1).map2(Left(2))(_ + _) == Left(2))
+  }
+  @Test def exercise4_6_left_map2_left: Unit = {
+    assert((Left(1):Either[Int, Int]).map2(Left(2))(_ + _) == Left(1))
+  }
 }
