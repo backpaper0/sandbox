0107eb86e872096ec7e184d911283bc543131cba
Taichi Uragami <backpaper0@gmail.com>
2015-09-27 13:26:48 +0900

    EXERCISE 4.1 Optionの各メソッド

--- fp-in-scala-exercise/src/main/scala/exercise-4.scala
+++ fp-in-scala-exercise/src/main/scala/exercise-4.scala
@@ -1,11 +1,24 @@
 package exercise4
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B]
-  def flatMap[B](f: A => Option[B]): Option[B]
-  def getOrElse[B >: A](default: => B): B
-  def orElse[B >: A](ob: => Option[B]): Option[B]
-  def filter(f: A => Boolean): Option[A]
+
+  //EXERCISE 4.1 (P.67)
+
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(value) => Some(f(value))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(value) => value
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if(f(a)) Some(a) else None)
 }
 
 case class Some[+A](get: A) extends Option[A]
--- fp-in-scala-exercise/src/test/scala/exercise-4-test.scala
+++ fp-in-scala-exercise/src/test/scala/exercise-4-test.scala
@@ -3,4 +3,41 @@ package exercise4
 import org.junit._
 
 class Exercise4Test {
+
+  @Test def exercise4_1_some_map: Unit = {
+    assert(Some(1).map(_ + 1) == Some(2))
+  }
+  @Test def exercise4_1_none_map: Unit = {
+    assert((None:Option[Int]).map(_ + 1) == None)
+  }
+  @Test def exercise4_1_some_flatMap_some: Unit = {
+    assert(Some(1).flatMap(a => Some(a + 1)) == Some(2))
+  }
+  @Test def exercise4_1_some_flatMap_none: Unit = {
+    assert(Some(1).flatMap(a => None) == None)
+  }
+  @Test def exercise4_1_none_flatMap: Unit = {
+    assert((None:Option[Int]).flatMap(a => Some(a + 1)) == None)
+  }
+  @Test def exercise4_1_some_getOrElse: Unit = {
+    assert(Some(1).getOrElse(2) == 1)
+  }
+  @Test def exercise4_1_none_getOrElse: Unit = {
+    assert((None:Option[Int]).getOrElse(2) == 2)
+  }
+  @Test def exercise4_1_some_orElse: Unit = {
+    assert(Some(1).orElse(Some(2)) == Some(1))
+  }
+  @Test def exercise4_1_none_orElse: Unit = {
+    assert((None:Option[Int]).orElse(Some(2)) == Some(2))
+  }
+  @Test def exercise4_1_some_filter_true: Unit = {
+    assert(Some(1).filter(_ => true) == Some(1))
+  }
+  @Test def exercise4_1_some_filter_false: Unit = {
+    assert(Some(1).filter(_ => false) == None)
+  }
+  @Test def exercise4_1_none_fitler: Unit = {
+    assert((None:Option[Int]).filter(_ => true) == None)
+  }
 }
