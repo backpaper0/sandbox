cca3953220bfb45e9acaf26cc6defac9da2ba23e
Taichi Uragami <backpaper0@gmail.com>
2015-09-27 20:59:40 +0900

    EXERCISE 4.8の準備
    と思ったけど4.8は後回しにしようかな( 'ω' )

--- fp-in-scala-exercise/src/main/scala/exercise-4-either.scala
+++ fp-in-scala-exercise/src/main/scala/exercise-4-either.scala
@@ -57,3 +57,20 @@ object Either {
     g(as, Nil)
   }
 }
+
+        case class Person(name: Name, age: Age)
+        sealed class Name(val value: String)
+        sealed class Age(val value: Int)
+
+        package object person {
+          def mkName(name: String): Either[String, Name] =
+            if(name == "" || name == null) Left("Name is empty.")
+            else Right(new Name(name))
+
+            def mkAge(age: Int): Either[String, Age] =
+              if(age < 0) Left("Age is out of range.")
+              else Right(new Age(age))
+
+              def mkPerson(name: String, age: Int): Either[String, Person] =
+                mkName(name).map2(mkAge(age))(Person(_, _))
+        }
