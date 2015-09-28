cbbd8fc9614eae49813c0a2862eaaab0414653b9
Taichi Uragami <backpaper0@gmail.com>
2015-09-28 21:48:23 +0900

    真偽値

--- lamda-calc-in-groovy/lambda.groovy
+++ lamda-calc-in-groovy/lambda.groovy
@@ -5,10 +5,17 @@ ONE   = { f -> { x -> f(x) }}
 TWO   = { f -> { x -> f(f(x)) }}
 THREE = { f -> { x -> f(f(f(x))) }}
 
+//真偽値
+TRUE  = { t -> { f -> t }}
+FALSE = { t -> { f -> f }}
+
 
 
 //intに変換する関数
 def toInt(n) { n({ it + 1 })(0) }
+
+//booleanに変換する関数
+def toBoolean(b) { b(true)(false) }
 
 
 
@@ -17,6 +24,10 @@ assert(toInt(ZERO)  == 0)
 assert(toInt(ONE)   == 1)
 assert(toInt(TWO)   == 2)
 assert(toInt(THREE) == 3)
+
+//真偽値
+assert(toBoolean(TRUE))
+assert(toBoolean(FALSE) == false)
 
 
 
