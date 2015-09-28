eb0cf58eedde54006dcab556576480084da7c0a4
Taichi Uragami <backpaper0@gmail.com>
2015-09-28 21:58:05 +0900

    リスト

--- lamda-calc-in-groovy/lambda.groovy
+++ lamda-calc-in-groovy/lambda.groovy
@@ -14,6 +14,13 @@ PAIR  = { l -> { r -> { f -> f(l)(r) }}}
 LEFT  = { p -> p(TRUE) }
 RIGHT = { p -> p(FALSE) }
 
+//リスト
+CONS   = { h -> { t -> PAIR(PAIR(FALSE)(h))(t) }}
+NIL    = PAIR(PAIR(TRUE)(TRUE))(TRUE)
+IS_NIL = { l -> LEFT(LEFT(l)) }
+HEAD   = { l -> RIGHT(LEFT(l)) }
+TAIL   = { l -> RIGHT(l) }
+
 
 
 //intに変換する関数
@@ -21,6 +28,17 @@ def toInt(n) { n({ it + 1 })(0) }
 
 //booleanに変換する関数
 def toBoolean(b) { b(true)(false) }
+
+//Listに変換する関数
+def toList(l, f) {
+    def xs = []
+    while(toBoolean(IS_NIL(l)) == false) {
+        def h = HEAD(l)
+        xs += f(h)
+        l = TAIL(l)
+    }
+    xs
+}
 
 
 
@@ -38,6 +56,10 @@ assert(toBoolean(FALSE) == false)
 def p1 = PAIR(ONE)(TWO)
 assert(toInt(LEFT(p1))  == 1)
 assert(toInt(RIGHT(p1)) == 2)
+
+//リスト
+def l1 = CONS(ONE)(CONS(TWO)(CONS(THREE)(NIL)))
+assert(toList(l1, { toInt(it) }) == [1, 2, 3 ])
 
 
 
