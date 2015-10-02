f22d154da801e41d991b258a7bafb3e073313218
Taichi Uragami <backpaper0@gmail.com>
2015-10-03 05:34:09 +0900

    型変数の指定方法を変えて冗長な記述を減らした

--- fp-in-scala-exercise/src/main/scala/exercise-5.scala
+++ fp-in-scala-exercise/src/main/scala/exercise-5.scala
@@ -28,11 +28,11 @@ trait Stream[+A] {
     case _ => z
   }
   def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
-  def takeWhile(p: A => Boolean): Stream[A] = foldRight(empty:Stream[A])((a, b) => if (p(a)) cons(a, b) else empty)
-  def map[B](f: A => B): Stream[B] = foldRight(empty:Stream[B])((a, b) => cons(f(a), b))
-  def filter(p: A => Boolean): Stream[A] = foldRight(empty:Stream[A])((a, b) => if(p(a)) cons(a, b) else b)
+  def takeWhile(p: A => Boolean): Stream[A] = foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else empty)
+  def map[B](f: A => B): Stream[B] = foldRight(empty[B])((a, b) => cons(f(a), b))
+  def filter(p: A => Boolean): Stream[A] = foldRight(empty[A])((a, b) => if(p(a)) cons(a, b) else b)
   def append[B >: A](s: Stream[B]): Stream[B] = foldRight(s)((a, b) => cons(a, b))
-  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty:Stream[B])((a, b) => f(a).append(b))
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((a, b) => f(a).append(b))
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
