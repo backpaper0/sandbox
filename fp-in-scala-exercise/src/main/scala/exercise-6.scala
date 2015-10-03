5ffb0bb901f51c79f12c46ed286cca130d1cbf50
Taichi Uragami <backpaper0@gmail.com>
2015-10-03 10:26:14 +0900

    EXERCISE 6.3 intDouble, doubleInt, double3

--- fp-in-scala-exercise/src/main/scala/exercise-6.scala
+++ fp-in-scala-exercise/src/main/scala/exercise-6.scala
@@ -23,4 +23,20 @@ object RNG {
     val (n, r) = nonNegativeInt(rng)
     (n.toDouble / Int.MaxValue.toDouble, r)
   }
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, rng2) = rng.nextInt
+    val (d, rng3) = double(rng2)
+    ((i, d), rng3)
+  }
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, rng2) = double(rng)
+    val (i, rng3) = rng2.nextInt
+    ((d, i), rng3)
+  }
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    ((d1, d2, d3), rng4)
+  }
 }
