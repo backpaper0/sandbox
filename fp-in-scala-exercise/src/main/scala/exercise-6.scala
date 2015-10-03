5bf156d7c6cb82b5c85fc31e9054a7144db20672
Taichi Uragami <backpaper0@gmail.com>
2015-10-03 10:01:50 +0900

    EXERCISE 6.xの準備

---
+++ fp-in-scala-exercise/src/main/scala/exercise-6.scala
@@ -0,0 +1,14 @@
+package exercise6
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5deece66dL + 0xbL) & 0xffffffffffffL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
