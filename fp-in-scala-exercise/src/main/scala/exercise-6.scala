15a0dc3478309828ff9d3e5bedaeace19945beea
Taichi Uragami <backpaper0@gmail.com>
2015-10-04 07:11:52 +0900

    EXERCISE 6.9 flatMapを使ってmap, map2

--- fp-in-scala-exercise/src/main/scala/exercise-6.scala
+++ fp-in-scala-exercise/src/main/scala/exercise-6.scala
@@ -43,16 +43,9 @@ object RNG {
   val int: Rand[Int] = _.nextInt
 
   def unit[A](a: A): Rand[A] = rng => (a, rng)
-  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
-    val (a, rng2) = s(rng)
-    (f(a), rng2)
-  }
+  def map[A, B](ra: Rand[A])(f: A => B): Rand[B] = flatMap(ra)(a => rng => (f(a), rng))
   def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
-    val (a, rng2) = ra(rng)
-    val (b, rng3) = rb(rng2)
-    (f(a, b), rng3)
-  }
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = flatMap(ra)(a => flatMap(rb)(b => rng => (f(a, b), rng)))
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = rng => {
     @annotation.tailrec
     def f(rng2: RNG, rs: List[Rand[A]], as: List[A]): (List[A], RNG) = rs match {
