b57890fd23c66f10ca035963ebe9bbcb76fbf1c0
Taichi Uragami <backpaper0@gmail.com>
2015-10-04 21:09:40 +0900

    modifyメソッドを追加した

--- fp-in-scala-exercise/src/main/scala/exercise-6.scala
+++ fp-in-scala-exercise/src/main/scala/exercise-6.scala
@@ -79,4 +79,8 @@ object State {
   })
   def get[S]: State[S, S] = State(s => (s, s))
   def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
