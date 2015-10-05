23a4d18cbdad05a79e62f810813ec4b88632f3ba
Taichi Uragami <backpaper0@gmail.com>
2015-10-05 22:24:54 +0900

    state.scalaにまとめた

--- fp-in-scala-exercise/scripts/state.scala
+++ fp-in-scala-exercise/scripts/state.scala
@@ -38,4 +38,43 @@ def succAndTwice2(n: Int): (Int, Int) = get[Int].flatMap { a =>
   }
 }.run(n)
 
-println(succAndTwice2(5))
+assert(succAndTwice2(5) == (5, 12))
+
+
+// Stack
+
+case class Stack[A](as: List[A]) {
+  def pop: (A, Stack[A]) = (as.head, Stack(as.tail))
+  def push(a: A): (Unit, Stack[A]) = ((), Stack(a :: as))
+}
+
+object Stack {
+  def empty[A]: Stack[A] = Stack(Nil)
+  def pop[A]: State[Stack[A], A] = State(s => s.pop)
+  def push[A](a: A): State[Stack[A], Unit] = State(s => s.push(a))
+}
+
+val stack1 = Stack.empty[Int]
+val (_, stack2) = stack1.push(1)
+val (_, stack3) = stack2.push(2)
+val (_, stack4) = stack3.push(3)
+val (a1, stack5) = stack4.pop
+val (a2, stack6) = stack5.pop
+val (a3, stack7) = stack6.pop
+val a = a1 * a2 + a3
+
+assert(a == 7)
+
+import Stack._
+
+val (b, _) = (for {
+  _ <- push(1)
+  _ <- push(2)
+  _ <- push(3)
+  b1 <- pop
+  b2 <- pop
+  b3 <- pop
+} yield b1 * b2 + b3).run(stack1)
+
+assert(b == 7)
+
