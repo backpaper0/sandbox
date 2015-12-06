4200b7b566d72150a0dc3cb1f3f7ccf4d8f2d9a9
Taichi Uragami <backpaper0@gmail.com>
2015-12-06 20:52:57 +0900

    セミコロンレスJavaでenumにメソッド生やすサンプル

---
+++ garakuta/src/main/java/SemicolonlessJavaEnumMethodSample.java
@@ -0,0 +1,24 @@
+import java.util.stream.Stream;
+
+public class SemicolonlessJavaEnumMethodSample {
+
+    enum Hoge implements Fuga {
+        FOO, BAR, BAZ
+    }
+
+    interface Fuga {
+        default void println() {
+            if (System.out.printf("%s%n", ((Hoge) this).name()) != null) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        if (Stream.of(0).peek(a -> Hoge.FOO.println()).count() > 0) {
+        }
+        if (Stream.of(0).peek(a -> Hoge.BAR.println()).count() > 0) {
+        }
+        if (Stream.of(0).peek(a -> Hoge.BAZ.println()).count() > 0) {
+        }
+    }
+}
