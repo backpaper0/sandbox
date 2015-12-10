9228bd73cf329727dd61df810d83fca63d330942
Taichi Uragami <backpaper0@gmail.com>
2015-12-11 05:47:47 +0900

    セミコロンレスJavaで1から100までのIntStreamを作るための試行錯誤

---
+++ garakuta/src/main/java/IntStreamForSemicolonlessJava.java
@@ -0,0 +1,41 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * セミコロンレスJavaで1から100までのIntStreamを作るための試行錯誤
+ *
+ */
+public class IntStreamForSemicolonlessJava {
+
+    public static void main(String[] args) throws Exception {
+
+        // https://twitter.com/bitter_fox/status/674438084474200066
+        String s1 = Optional.of(SecureRandom.getInstanceStrong())
+                .map(r -> IntStream.generate(() -> r.nextInt(101)).distinct()
+                        .limit(101).sorted().skip(1))
+                .get()
+                //
+                .mapToObj(String::valueOf).collect(Collectors.joining(" "));
+        System.out.println(s1);
+
+        String s2 = Stream
+                .of((ArrayList) Collectors.toList().supplier().get()).peek(
+                        list -> list.add(BigInteger.ONE))
+                .map(list -> IntStream
+                        .generate(
+                                () -> Stream.of((BigInteger) list.get(0))
+                                        .peek(a -> list.remove(0))
+                                        .peek(a -> list
+                                                .add(a.add(BigInteger.ONE)))
+                                .findFirst().get().intValue()))
+                .findFirst().get().limit(100)
+                //
+                .mapToObj(String::valueOf).collect(Collectors.joining(" "));
+        System.out.println(s2);
+    }
+}
