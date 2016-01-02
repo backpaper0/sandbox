e516c5538c6cebaa8315f4c1e818cdfae78b533d
Taichi Uragami <backpaper0@gmail.com>
2016-01-02 11:32:58 +0900

    CDIを使いたい

---
+++ wildfly-swarm-getting-started/src/main/java/app/HelloService.java
@@ -0,0 +1,6 @@
+package app;
+
+public interface HelloService {
+
+    String sayHello(String name);
+}
--- wildfly-swarm-getting-started/src/main/java/app/JaxrsActivator.java
+++ wildfly-swarm-getting-started/src/main/java/app/JaxrsActivator.java
@@ -1,8 +1,10 @@
 package app;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@ApplicationScoped
 @ApplicationPath("api")
 public class JaxrsActivator extends Application {
 }
