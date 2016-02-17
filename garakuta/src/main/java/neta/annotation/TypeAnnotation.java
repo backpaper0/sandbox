c6cd85fb45a64fef4f7b26d099f114be39c898e5
Taichi Uragami <backpaper0@gmail.com>
2016-02-17 20:36:15 +0900

    型アノテーションのサンプル……？
    もう何がなんだか……:(；ﾞﾟ'ωﾟ'):

---
+++ garakuta/src/main/java/neta/annotation/TypeAnnotation.java
@@ -0,0 +1,31 @@
+package neta.annotation;
+
+import java.io.Serializable;
+import java.io.UncheckedIOException;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Hoge
+public class TypeAnnotation<@Hoge A, @Hoge B> extends @Hoge ArrayList<@Hoge String>
+        implements @Hoge Serializable {
+
+    <@Hoge C> @Hoge Set<@Hoge String> method(@Hoge Map<@Hoge String, @Hoge List<@Hoge String>> map)
+            throws @Hoge IllegalArgumentException, @Hoge UncheckedIOException {
+        @Hoge
+        List<@Hoge String> list = new @Hoge ArrayList<@Hoge String>();
+        Arrays.<@Hoge Number> asList(1, 2, 3);
+        return null;
+    }
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+@interface Hoge {
+}
