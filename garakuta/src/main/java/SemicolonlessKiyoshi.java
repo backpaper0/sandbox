a7bf8fe444116ffe0e8a4fe6be80feafbd5167b2
Taichi Uragami <backpaper0@gmail.com>
2016-03-16 21:55:10 +0900

    セミコロンレスキヨシ

---
+++ garakuta/src/main/java/SemicolonlessKiyoshi.java
@@ -0,0 +1,12 @@
+
+public interface SemicolonlessKiyoshi {
+
+    public static void main(String[] args) {
+        if (java.util.stream.Stream.of(new java.util.Random())
+                .map(r -> (java.util.function.Supplier<String>) () -> r.nextBoolean() ? "ズン" : "ドコ")
+                .flatMap(r -> java.util.stream.Stream.iterate(r.get(), s -> s + r.get())
+                        .filter(s -> s.endsWith("ズンズンズンズンドコ")).map(s -> s + "キ・ヨ・シ！").limit(1))
+                .peek(System.out::println).count() > 0) {
+        }
+    }
+}
