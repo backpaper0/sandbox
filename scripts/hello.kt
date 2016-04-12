1af44673ea6a574fc16bc0af7f35cea4e46cec3f
Taichi Uragami <backpaper0@gmail.com>
2016-04-13 07:17:20 +0900

    拡張関数とかinvokeオペレーター

--- scripts/hello.kt
+++ scripts/hello.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
     println("Hello, korlin!")
+    "Hello, kotlin!".println()
+    val hello = Hello()   
+    hello()
+}
+
+fun String.println() = println(this)
+
+class Hello {
+    operator fun invoke() = println("Hello, kotlin!")
 }
