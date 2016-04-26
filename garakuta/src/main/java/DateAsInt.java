5456b51bcc32b30cc4495bc4f79f2bdcc78f211d
Taichi Uragami <backpaper0@gmail.com>
2016-04-27 06:11:19 +0900

    LocalDateをintに変換したり戻したりする何か

---
+++ garakuta/src/main/java/DateAsInt.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+public class DateAsInt {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        int i = toInt(date);
+        System.out.println(i);
+        date = fromInt(i);
+        System.out.println(date);
+    }
+
+    static int toInt(LocalDate date) {
+        return (date.getYear() * 10000) + (date.getMonthValue() * 100) + date.getDayOfMonth();
+    }
+
+    static LocalDate fromInt(int i) {
+        int src = i;
+        int year = src / 10000;
+        src = src - (year * 10000);
+        int month = src / 100;
+        src = src - (month * 100);
+        int dayOfMonth = src;
+        return LocalDate.of(year, month, dayOfMonth);
+    }
+}
