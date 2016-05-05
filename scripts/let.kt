f294b64f8a070dc099185c54f742cecf55978f72
Taichi Uragami <backpaper0@gmail.com>
2016-05-05 22:29:53 +0900

    関数のチェーンを考えてみた

--- scripts/let.kt
+++ scripts/let.kt
@@ -4,6 +4,20 @@ fun main(args: Array<String>) {
 
     val s2: String? = null
     s2?.let { p(it) }
+
+    p2(twice(len("hello")))
+
+    "hello".let { len(it).let { twice(it).let { p2(it) } } }
+
+    "hello" x { len(it) } x { twice(it) } x { p2(it) }
 }
 
 fun p(s: String) = println(s)
+
+fun len(s: String): Int = s.length
+
+fun twice(i: Int): Int = i * 2
+
+fun p2(i: Int) = println(i)
+
+infix fun <T, U> T.x(f: (T) -> U): U = f(this)
