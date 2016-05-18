98a6995f398df17872540b542fdfc838efbddd18
Taichi Uragami <backpaper0@gmail.com>
2016-05-18 23:52:45 +0900

    Optionをいい感じにアレするやつが上手くいかなくてアレ(・_・)

--- jaxrs-scala-sample/src/main/scala/jaxrs.scala
+++ jaxrs-scala-sample/src/main/scala/jaxrs.scala
@@ -13,3 +13,23 @@ class HelloApi {
   @GET
   def say(@QueryParam("name") name: String, @Context uriInfo: UriInfo): String = s"Hello, ${name}! : ${uriInfo.getAbsolutePath()}"
 }
+
+@Path("echo")
+class EchoApi {
+  @POST
+  @Consumes(Array("application/x-www-form-urlencoded"))
+  @Produces(Array("text/plain"))
+  def echo(@FormParam("data") data: String): Option[String] = Option(data)
+}
+
+@Provider
+class OptionProvider extends WriterInterceptor {
+  def aroundWriteTo(ctx: WriterInterceptorContext) {
+    ctx.getEntity match {
+      case Some(entity) => ctx.setEntity(entity)
+      case None => ctx.setEntity(null)
+      case _ =>
+    }
+    ctx.proceed()
+  }
+}
