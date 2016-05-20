215fe1614e2b28daf4329e250dc86586d128bcbe
Taichi Uragami <backpaper0@gmail.com>
2016-05-20 21:41:51 +0900

    ･:*+.\(( °ω° ))/.:+

--- js-promise-sample/promise-heaven.js
+++ js-promise-sample/promise-heaven.js
@@ -1,58 +1,21 @@
-function h() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('h');
-            resolve();
-        }, 100);
-    });
+function word(x) {
+    return function h() {
+        return new Promise(function(resolve) {
+            setTimeout(function() {
+                console.log(x);
+                resolve();
+            }, 100);
+        });
+    }
 }
-function e() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('e');
-            resolve();
-        }, 100);
-    });
-}
-function l() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('l');
-            resolve();
-        }, 100);
-    });
-}
-function o() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('o');
-            resolve();
-        }, 100);
-    });
-}
-function w() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('w');
-            resolve();
-        }, 100);
-    });
-}
-function r() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('r');
-            resolve();
-        }, 100);
-    });
-}
-function d() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('d');
-            resolve();
-        }, 100);
-    });
-}
+
+var h = word('h');
+var e = word('e');
+var l = word('l');
+var o = word('o');
+var w = word('w');
+var r = word('r');
+var d = word('d');
+
 h().then(e).then(l).then(l).then(o).then(w).then(o).then(r).then(l).then(d)
 
