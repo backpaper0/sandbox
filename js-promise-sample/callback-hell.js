1a7dab89c89d00138743534bc5b9c336784c0fcd
Taichi Uragami <backpaper0@gmail.com>
2016-05-20 21:39:16 +0900

    コールバック地獄😇

---
+++ js-promise-sample/callback-hell.js
@@ -0,0 +1,30 @@
+setTimeout(function() {
+    console.log('h');
+    setTimeout(function() {
+        console.log('e');
+        setTimeout(function() {
+            console.log('l');
+            setTimeout(function() {
+                console.log('l');
+                setTimeout(function() {
+                    console.log('o');
+                    setTimeout(function() {
+                        console.log('w');
+                        setTimeout(function() {
+                            console.log('o');
+                            setTimeout(function() {
+                                console.log('r');
+                                setTimeout(function() {
+                                    console.log('l');
+                                    setTimeout(function() {
+                                        console.log('d');
+                                    }, 100);
+                                }, 100);
+                            }, 100);
+                        }, 100);
+                    }, 100);
+                }, 100);
+            }, 100);
+        }, 100);
+    }, 100);
+}, 100);
