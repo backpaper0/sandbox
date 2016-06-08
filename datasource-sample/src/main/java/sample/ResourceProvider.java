9689b0f89f5decbd05a5a7312e086c4bcf3a760d
Taichi Uragami <backpaper0@gmail.com>
2016-06-08 21:49:36 +0900

    DataSourceのプロバイダにDataSourceDefinitionを付けてみる

--- datasource-sample/src/main/java/sample/ResourceProvider.java
+++ datasource-sample/src/main/java/sample/ResourceProvider.java
@@ -1,11 +1,13 @@
 package sample;
 
 import javax.annotation.Resource;
+import javax.annotation.sql.DataSourceDefinition;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.sql.DataSource;
 
 @Dependent
+@DataSourceDefinition(name = "java:global/jdbc/sample", className = "org.h2.jdbcx.JdbcDataSource", url = "${ds.url}", user = "sa", password = "secret")
 public class ResourceProvider {
 
     @Resource(lookup = "java:global/jdbc/sample")
--- datasource-sample/src/main/java/sample/SampleApp.java
+++ datasource-sample/src/main/java/sample/SampleApp.java
@@ -1,12 +1,10 @@
 package sample;
 
-import javax.annotation.sql.DataSourceDefinition;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationScoped
 @ApplicationPath("api")
-@DataSourceDefinition(name = "java:global/jdbc/sample", className = "org.h2.jdbcx.JdbcDataSource", url = "${ds.url}", user = "sa", password = "secret")
 public class SampleApp extends Application {
 }
