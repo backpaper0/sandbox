332f41a1565f7073f2ede1e1dcc1b41a8897dbc0
Taichi Uragami <backpaper0@gmail.com>
2016-06-16 07:54:22 +0900

    REST API風にCRUD

--- simple-javaee-crud-sample/src/main/java/sample/Sample.java
+++ simple-javaee-crud-sample/src/main/java/sample/Sample.java
@@ -5,8 +5,10 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQuery(name = "Sample.findAll", query = "SELECT a FROM Sample a")
 public class Sample implements Serializable {
 
     private Long id;
@@ -28,5 +30,10 @@ public class Sample implements Serializable {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s", id, text);
     }
 }
--- simple-javaee-crud-sample/src/main/java/sample/SampleResource.java
+++ simple-javaee-crud-sample/src/main/java/sample/SampleResource.java
@@ -1,8 +1,17 @@
 package sample;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -10,9 +19,35 @@ import javax.ws.rs.core.MediaType;
 @Path("sample")
 public class SampleResource {
 
+    @Inject
+    private SampleService service;
+
+    @Path("{id}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String get() {
-        return "hello";
+    public String get(@PathParam("id") Long id) {
+        return service.get(id).map(Objects::toString).orElseThrow(NotFoundException::new);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String findAll() {
+        return service.findAll().stream().map(Objects::toString)
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String create(@FormParam("text") String text) {
+        return service.create(text).toString();
+    }
+
+    @Path("{id}")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String create(@PathParam("id") Long id, @FormParam("text") String text) {
+        return service.update(id, text).map(Objects::toString).orElseThrow(NotFoundException::new);
     }
 }
