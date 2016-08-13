a4b5375eed5011a7c2529661cd721a451a8ca6e6
Taichi Uragami <backpaper0@gmail.com>
2016-08-13 09:27:30 +0900

    ::を使わないで説明してみる

--- scripts/colon-colon.scala
+++ scripts/colon-colon.scala
@@ -1,13 +1,26 @@
+
+//Intのペアを表現する :::::::: を定義して説明してみる
+
+class ::::::::(val x: Int, val y: Int)
+
+//コンパニオンオブジェクト
 object :::::::: {
-  def unapply[A](xs: ::[A]): Some[(A, List[A])] = Some((xs.head, xs.tail))
+  //Some((x, y))を返すunapplyメソッドがあれば中置記法が使える
+  def unapply(a: ::::::::) = Some((a.x, a.y))
 }
 
-val xs = 1 to 5 toList
+val a = new ::::::::(123, 456)
 
-val hoge = xs match {
-  case h :::::::: _ => h
-  case _ => -1
+//中置記法
+val b = a match {
+  case x :::::::: _ => x
 }
 
-println(hoge)
+//中置記法を使わない場合はこんな感じ
+val c = a match {
+  case ::::::::(_, y) => y
+}
+
+println(b)
+println(c)
 
