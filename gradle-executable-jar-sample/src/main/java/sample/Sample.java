b7d0d31952894a654688fe17a08da91ea344c425
Taichi Uragami <backpaper0@gmail.com>
2016-09-08 18:10:52 +0900

    Spring BootプラギンでUberJAR作ったらクラスローダをアレされる

--- gradle-executable-jar-sample/src/main/java/sample/Sample.java
+++ gradle-executable-jar-sample/src/main/java/sample/Sample.java
@@ -3,5 +3,6 @@ package sample;
 public class Sample {
     public static void main(String args[]) {
         System.out.println("Hello, world!");
+        System.out.println(Sample.class.getClassLoader());
     }
 }
