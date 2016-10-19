a869bafd70876a55f6472e96264456def65b1239
Taichi Uragami <backpaper0@gmail.com>
2016-10-19 22:55:48 +0900

    インクリメント

--- scripts/type-level-programming/int.scala
+++ scripts/type-level-programming/int.scala
@@ -1,16 +1,26 @@
 import scala.language.higherKinds
 
-sealed trait int
+sealed trait int {
+  type increment <: int
+}
 
-sealed trait _0 extends int
+sealed trait _0 extends int {
+  type increment = succ[_0]
+}
 
-sealed trait succ[A <: int] extends int
+sealed trait succ[A <: int] extends int {
+  type increment = succ[succ[A]]
+}
 
 type _1 = succ[_0]
 
 type _2 = succ[_1]
 
 type _3 = succ[_2]
+
+object int {
+  type ++[A <: int] = A#increment
+}
 
 def toInt[A <: int](implicit x: ToInt[A]) = x()
 
@@ -26,7 +36,12 @@ implicit def toSucc[B <: int](implicit x: ToInt[B]) = new ToInt[succ[B]] {
   def apply() = 1 + x()
 }
 
+import int._
+
 println(toInt[_0])
 println(toInt[_1])
 println(toInt[_2])
 println(toInt[_3])
+
+println("++3 = " + toInt[++[_3]])
+
