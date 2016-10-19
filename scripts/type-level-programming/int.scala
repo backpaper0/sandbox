20d2470b50f002b2854f1819dce4a7808ec1f43d
Taichi Uragami <backpaper0@gmail.com>
2016-10-19 23:07:09 +0900

    a + b

--- scripts/type-level-programming/int.scala
+++ scripts/type-level-programming/int.scala
@@ -2,14 +2,26 @@ import scala.language.higherKinds
 
 sealed trait int {
   type increment <: int
+  type add[A <: int] <: int
 }
 
 sealed trait _0 extends int {
   type increment = succ[_0]
+  type add[A <: int] = A
 }
 
 sealed trait succ[A <: int] extends int {
   type increment = succ[succ[A]]
+  type add[B <: int] = B#add[A]#increment
+  // _2#add[_1]
+  // succ[_1]#add[_1]
+  // _1#add[_1]#increment
+  // succ[_0]#add[_1]#increment
+  // _1#add[_0]#increment#increment
+  // succ[_0]#add[_0]#increment#increment
+  // _0#add[_0]#increment#increment#increment
+  // _0#increment#increment#increment
+  // _3
 }
 
 type _1 = succ[_0]
@@ -20,6 +32,7 @@ type _3 = succ[_2]
 
 object int {
   type ++[A <: int] = A#increment
+  type +[A <: int, B <: int] = A#add[B]
 }
 
 def toInt[A <: int](implicit x: ToInt[A]) = x()
@@ -45,3 +58,4 @@ println(toInt[_3])
 
 println("++3 = " + toInt[++[_3]])
 
+println("2 + 3 = " + toInt[_2 + _3])
