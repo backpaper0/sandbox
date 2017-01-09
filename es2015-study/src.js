f3c86474d7fabab6850e10716638c8220b5226ab
Taichi Uragami <backpaper0@gmail.com>
2017-01-10 00:10:09 +0900

    es2015を学びたい

---
+++ es2015-study/src.js
@@ -0,0 +1,28 @@
+const obj = {
+  foo: [1, 2, 3],
+  bar: [4, 5, 6],
+  baz: [7, 8, 9]
+};
+
+const a = Object.values(obj).map(x => x[0]);
+
+console.log(a);
+
+
+const arr = ['foo', 'bar'];
+
+arr.forEach(x => console.log(x));
+
+class Person {
+  constructor(name) {
+    this._name = name
+  }
+  get name() { return this._name }
+  hello() {
+    return 'Hello, ' + this.name + '!'
+  }
+}
+
+const p = new Person('backpaper0')
+console.log(p.name)
+console.log(p.hello())
