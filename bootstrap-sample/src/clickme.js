cbe86603eae8f2460d07f94620cb472060770979
Taichi Uragami <backpaper0@gmail.com>
2017-01-14 17:19:52 +0900

    いろいろ遊んでみた

---
+++ bootstrap-sample/src/clickme.js
@@ -0,0 +1,10 @@
+
+export const hello = 'hello world!!!!!!!'
+
+export const hoge = () => 'xxxxxxxxxxxxxxxx'
+
+export default function() {
+  document.getElementById('clickme').addEventListener('click', function() {
+   console.log('hello world')
+  })
+}
---
+++ bootstrap-sample/webpack.config.js
@@ -0,0 +1,64 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(otf|eot|svg|ttf|woff|woff2)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+//    new webpack.optimize.UglifyJsPlugin({
+//      sourceMap: true,
+//      compress: {
+//        warnings: false
+//      }
+//    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
