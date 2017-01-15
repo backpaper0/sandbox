eace1e4f7021159b004963410e7a2f90f4153b40
Taichi Uragami <backpaper0@gmail.com>
2017-01-15 22:59:23 +0900

    SUCCを追加

--- scripts/lambda.js
+++ scripts/lambda.js
@@ -3,10 +3,13 @@ const ONE   = f => s => f(s);
 const TWO   = f => s => f(f(s));
 const THREE = f => s => f(f(f(s)));
 
+const SUCC = n => f => s => n(f)(f(s));
+
 const toInt = n => n(x => x + 1)(0);
 
 console.log(toInt(ZERO));
 console.log(toInt(ONE));
 console.log(toInt(TWO));
 console.log(toInt(THREE));
+console.log(toInt(SUCC(THREE)));
 
