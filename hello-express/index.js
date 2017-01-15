b3f1e9e3a1d914df79e3dc841df38d2da2b425ff
Taichi Uragami <backpaper0@gmail.com>
2017-01-15 22:13:29 +0900

    ミドルウェアを作ってみる

--- hello-express/index.js
+++ hello-express/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello', function(req, res) {
+const logger = (req, res, next) => {
+  console.log('hoge');
+  next();
+};
+
+app.get('/hello', (req, res) => {
   res.send('hello world')
+});
+
+app.use(logger);
+
+app.get('/foobar', function(req, res) {
+  res.send('foobar')
 });
 
 app.listen(3000);
