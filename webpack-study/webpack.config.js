5a558e7bf4a218b23d2056741fe00864968c7688
Taichi Uragami <backpaper0@gmail.com>
2017-01-24 23:24:42 +0900

    webpackを学ぶ

---
+++ webpack-study/webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, '.'),
+    publicPath: '/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    contentBase: './www'
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
