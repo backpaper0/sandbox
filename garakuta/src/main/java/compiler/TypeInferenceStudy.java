a854f2ad20156980b4787cb69ccc8d6c455d35c6
Taichi Uragami <backpaper0@gmail.com>
2017-03-05 20:15:13 +0900

    let

--- garakuta/src/main/java/compiler/TypeInferenceStudy.java
+++ garakuta/src/main/java/compiler/TypeInferenceStudy.java
@@ -1,18 +1,26 @@
 package compiler;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import compiler.TypeInferenceStudy.Expr.BinOp;
-import compiler.TypeInferenceStudy.Expr.BinOpType;
-import compiler.TypeInferenceStudy.Expr.Bool;
-import compiler.TypeInferenceStudy.Expr.Id;
-import compiler.TypeInferenceStudy.Expr.IfThenElse;
-import compiler.TypeInferenceStudy.Expr.Int;
+import java.util.Queue;
+import compiler.TypeInferenceStudy.Ast.BinOp;
+import compiler.TypeInferenceStudy.Ast.BinOpType;
+import compiler.TypeInferenceStudy.Ast.Bool;
+import compiler.TypeInferenceStudy.Ast.Expr;
+import compiler.TypeInferenceStudy.Ast.Id;
+import compiler.TypeInferenceStudy.Ast.IfThenElse;
+import compiler.TypeInferenceStudy.Ast.Int;
+import compiler.TypeInferenceStudy.Ast.Let;
+import compiler.TypeInferenceStudy.Ast.Let2;
 
 /**
  * @see http://www.fos.kuis.kyoto-u.ac.jp/~igarashi/class/isle4-06w/text/miniml006.html
+ * @see http://www.fos.kuis.kyoto-u.ac.jp/~igarashi/class/isle4-06w/text/miniml007.html
  */
 public class TypeInferenceStudy {
 
@@ -25,20 +33,24 @@ public class TypeInferenceStudy {
         eval("1 + 2 * 3 + 4;;");
         eval("true || false;;");
         eval("1 < 2 && true && 3 < x1;;");
+        eval("let x = 123;;");
+        eval("let a = 10 in a + 1;;");
     }
 
     private static void eval(String source) {
+        System.out.println(source);
         Lexer lexer = new Lexer(source);
         Parser parser = new Parser(lexer);
-        Expr expr = parser.parse();
+        Ast ast = parser.parse();
         Evaluator evaluator = new Evaluator();
         try {
-            System.out.println(expr);
-            expr.accept(evaluator);
+            System.out.println(ast);
+            ast.accept(evaluator);
             System.out.println(evaluator.getResultValue());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        System.out.println();
     }
 
     static class Evaluator implements Expr.Visitor {
@@ -107,11 +119,24 @@ public class TypeInferenceStudy {
                 expr.else_.accept(this);
             }
         }
+
+        @Override
+        public void visit(Let expr) {
+            expr.expr1.accept(this);
+            env.put(expr.id.name, stack.pop());
+            expr.expr2.accept(this);
+        }
+
+        @Override
+        public void visit(Let2 expr) {
+            expr.expr.accept(this);
+            env.put(expr.id.name, stack.pop());
+        }
     }
 
     enum TokenType {
         EOF, IF, THEN, ELSE, BOOL, L_PAREN, R_PAREN, ADD_OP, MUL_OP, LT_OP, INT, ID, SEMICOLON,
-        AND_OP, OR_OP;
+        AND_OP, OR_OP, LET, EQ_OP, IN;
     }
 
     static class Token {
@@ -188,6 +213,9 @@ public class TypeInferenceStudy {
                     return new Token(TokenType.OR_OP, "||");
                 }
                 throw new RuntimeException();
+            case '=':
+                consume();
+                return new Token(TokenType.EQ_OP, "=");
             default:
                 if ('0' <= c && c <= '9') {
                     StringBuilder buf = new StringBuilder();
@@ -214,6 +242,10 @@ public class TypeInferenceStudy {
                     case "true":
                     case "false":
                         return new Token(TokenType.BOOL, text);
+                    case "let":
+                        return new Token(TokenType.LET, text);
+                    case "in":
+                        return new Token(TokenType.IN, text);
                     }
                     return new Token(TokenType.ID, text);
                 }
@@ -222,7 +254,9 @@ public class TypeInferenceStudy {
         }
     }
 
-    interface Expr {
+    interface Ast {
+        interface Expr extends Ast {
+        }
 
         static class Id implements Expr {
             String name;
@@ -311,10 +345,48 @@ public class TypeInferenceStudy {
             }
         }
 
+        static class Let implements Expr {
+            Id id;
+            Expr expr1;
+            Expr expr2;
+            public Let(Id id, Expr expr1, Expr expr2) {
+                this.id = id;
+                this.expr1 = expr1;
+                this.expr2 = expr2;
+            }
+            @Override
+            public void accept(Visitor visitor) {
+                visitor.visit(this);
+            }
+            @Override
+            public String toString() {
+                return String.format("LET(%s, %s, %s)", id, expr1, expr2);
+            }
+        }
+
+        static class Let2 implements Ast {
+            Id id;
+            Expr expr;
+            public Let2(Id id, Expr expr) {
+                this.id = id;
+                this.expr = expr;
+            }
+            @Override
+            public void accept(Visitor visitor) {
+                visitor.visit(this);
+            }
+            @Override
+            public String toString() {
+                return String.format("LET(%s, %s)", id, expr);
+            }
+        }
+
         void accept(Visitor visitor);
 
         interface Visitor {
             void visit(Id expr);
+            void visit(Let expr);
+            void visit(Let2 expr);
             void visit(Int expr);
             void visit(Bool expr);
             void visit(BinOp expr);
@@ -325,31 +397,60 @@ public class TypeInferenceStudy {
     static class Parser {
         Lexer lexer;
         Token token;
+        Queue<Token> tokens = new LinkedList<>();
+        List<Token> rollback = new ArrayList<>();
         public Parser(Lexer lexer) {
             this.lexer = lexer;
             consume();
         }
         private void consume() {
-            token = lexer.nextToken();
+            token = tokens.poll();
+            if (token == null) {
+                token = lexer.nextToken();
+            }
+            rollback.add(token);
         }
-        Expr parse() {
-            Expr expr = expr();
-            match(TokenType.SEMICOLON);
-            match(TokenType.SEMICOLON);
-            match(TokenType.EOF);
-            return expr;
+        void rollback() {
+            tokens.addAll(rollback);
+            rollback.clear();
+            consume();
         }
-        private void match(TokenType expected) {
+        void commit() {
+            rollback.clear();
+        }
+        Ast parse() {
+            try {
+                try {
+                    Expr expr = expr();
+                    match(TokenType.SEMICOLON);
+                    match(TokenType.SEMICOLON);
+                    match(TokenType.EOF);
+                    commit();
+                    return expr;
+                } catch (ParseException e) {
+                    rollback();
+                }
+                Let2 let = let2();
+                match(TokenType.SEMICOLON);
+                match(TokenType.SEMICOLON);
+                match(TokenType.EOF);
+                commit();
+                return let;
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        private void match(TokenType expected) throws ParseException {
             if (token.tokenType == expected) {
                 consume();
             } else {
-                throw new RuntimeException();
+                throw new ParseException("", 0);
             }
         }
-        Expr expr() {
+        Expr expr() throws ParseException {
             return or();
         }
-        Expr or() {
+        Expr or() throws ParseException {
             Expr left = and();
             while (token.tokenType == TokenType.OR_OP) {
                 consume();
@@ -358,7 +459,7 @@ public class TypeInferenceStudy {
             }
             return left;
         }
-        Expr and() {
+        Expr and() throws ParseException {
             Expr left = lessThan();
             while (token.tokenType == TokenType.AND_OP) {
                 consume();
@@ -367,7 +468,7 @@ public class TypeInferenceStudy {
             }
             return left;
         }
-        Expr lessThan() {
+        Expr lessThan() throws ParseException {
             Expr left = add();
             while (token.tokenType == TokenType.LT_OP) {
                 consume();
@@ -376,7 +477,7 @@ public class TypeInferenceStudy {
             }
             return left;
         }
-        Expr add() {
+        Expr add() throws ParseException {
             Expr left = mul();
             while (token.tokenType == TokenType.ADD_OP) {
                 consume();
@@ -385,7 +486,7 @@ public class TypeInferenceStudy {
             }
             return left;
         }
-        Expr mul() {
+        Expr mul() throws ParseException {
             Expr left = other();
             while (token.tokenType == TokenType.MUL_OP) {
                 consume();
@@ -394,7 +495,7 @@ public class TypeInferenceStudy {
             }
             return left;
         }
-        Expr other() {
+        Expr other() throws ParseException {
             switch (token.tokenType) {
             case BOOL: {
                 boolean value = Boolean.parseBoolean(token.text);
@@ -402,9 +503,7 @@ public class TypeInferenceStudy {
                 return new Bool(value);
             }
             case ID: {
-                String name = token.text;
-                consume();
-                return new Id(name);
+                return id();
             }
             case IF: {
                 consume();
@@ -426,9 +525,32 @@ public class TypeInferenceStudy {
                 match(TokenType.R_PAREN);
                 return expr;
             }
-            default:
-                throw new RuntimeException();
+            case LET: {
+                consume();
+                Id id = id();
+                match(TokenType.EQ_OP);
+                Expr expr1 = expr();
+                match(TokenType.IN);
+                Expr expr2 = expr();
+                return new Let(id, expr1, expr2);
             }
+            default:
+                throw new ParseException("", 0);
+            }
+        }
+
+        Id id() throws ParseException {
+            String name = token.text;
+            match(TokenType.ID);
+            return new Id(name);
+        }
+
+        Let2 let2() throws ParseException {
+            match(TokenType.LET);
+            Id id = id();
+            match(TokenType.EQ_OP);
+            Expr expr = expr();
+            return new Let2(id, expr);
         }
     }
 }
