e8b95b51318a3bda5ac6674b1c4f01b9e8349eff
Taichi Uragami <backpaper0@gmail.com>
2017-06-12 23:17:27 +0900

    デフォルト値

---
+++ typescript-study/src/default-value.ts
@@ -0,0 +1,1 @@
+export default (name: string = 'world') => `Hello, ${name}!`
--- typescript-study/src/main.ts
+++ typescript-study/src/main.ts
@@ -1,11 +1,15 @@
 import hello from './hello'
 import obj from './object'
+import defaultValue from './default-value'
 
 console.log(hello('world'))
 
 console.log(obj.name)
 console.log(obj.data().foo)
 console.log(obj.data().bar)
+
+console.log(defaultValue())
+console.log(defaultValue('backpaper0'))
 
 //compile error
 //console.log(hello(123))
