1d367bbeef012b19d01a38c0ea93a7f0adbb5cfa
Taichi Uragami <backpaper0@gmail.com>
2018-02-03 15:17:28 +0900

    2.1-2 非増加順

--- algorithm-introduction/src/insertion-sort.js
+++ algorithm-introduction/src/insertion-sort.js
@@ -10,3 +10,17 @@ export default a => {
         a[i + 1] = key;
     }
 };
+
+export const insertionSort2 = function* (a) {
+    for (let j = a.length - 2; j >= 0; j--) {
+        const key = a[j];
+        let i = j + 1;
+        while (j < a.length && a[i] < key) {
+            a[i - 1] = a[i];
+            yield;
+            i = i + 1;
+        }
+        a[i - 1] = key;
+        yield;
+    }
+};
--- algorithm-introduction/src/insertion-sort.test.js
+++ algorithm-introduction/src/insertion-sort.test.js
@@ -1,4 +1,4 @@
-import sort from './insertion-sort';
+import sort, { insertionSort2 } from './insertion-sort';
 
 test('ソートされる', () => {
     const a = [5, 2, 4, 6, 1, 3];
@@ -16,4 +16,47 @@ test('長さが1の配列', () => {
     const a = [0];
     sort(a);
     expect(a).toEqual([0]);
+});
+
+describe('2.1-2 非増加順', () => {
+
+    test('非増加順でソートされる', () => {
+        const a = [5, 2, 4, 6, 1, 3];
+        const iterator = insertionSort2(a);
+
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 2, 4, 6, 1, 3]);
+
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 2, 4, 1, 1, 3]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 2, 4, 1, 3, 3]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 2, 4, 1, 3, 6]);
+
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 2, 1, 1, 3, 6]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 2, 1, 3, 3, 6]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 2, 1, 3, 4, 6]);
+
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 1, 1, 3, 4, 6]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([5, 1, 2, 3, 4, 6]);
+
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([1, 1, 2, 3, 4, 6]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([1, 2, 2, 3, 4, 6]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([1, 2, 3, 3, 4, 6]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([1, 2, 3, 4, 4, 6]);
+        expect(iterator.next().done).toBeFalsy();
+        expect(a).toEqual([1, 2, 3, 4, 5, 6]);
+
+        expect(iterator.next().done).toBeTruthy();
+    });
 });
