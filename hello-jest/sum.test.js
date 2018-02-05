cf87469bc4cb2e2a1766b0bf6a23f72f206d2fdb
Taichi Uragami <backpaper0@gmail.com>
2018-02-05 21:27:12 +0900

    union typeとかgenericsとか

--- hello-jest/sum.test.js
+++ hello-jest/sum.test.js
@@ -1,5 +1,10 @@
-import sum from './sum';
+import sum, { fn1 } from './sum';
 
 test('2 + 3 = 5', () => {
   expect(sum(2, 3)).toBe(5);
+});
+
+describe('fn1', () => {
+  test('foo', () => expect(fn1('foo')).toEqual('FOO'));
+  test('bar', () => expect(fn1('bar')).toEqual('BAR'));
 });
