cfc55f2cdc8a073c7ba7549fbbfd140451436d3f
Taichi Uragami <backpaper0@gmail.com>
2018-02-20 07:33:35 +0900

    mvc:annotation-drivenを使ってみた

---
+++ springmvc-study-annotation-driven/src/main/java/com/example/HelloController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String handleRequest() {
+        return "Hello, world!!!!!";
+    }
+}
