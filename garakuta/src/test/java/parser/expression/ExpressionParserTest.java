d52da6e07d22b47c4358b48c277f103751227cfe
Taichi Uragami <backpaper0@gmail.com>
2018-05-13 21:43:44 +0900

    MethodSourceはArgumentsで複数の引数を設定できた

--- garakuta/src/test/java/parser/expression/ExpressionParserTest.java
+++ garakuta/src/test/java/parser/expression/ExpressionParserTest.java
@@ -6,6 +6,7 @@ import static parser.expression.ExpressionNode.*;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,12 +14,12 @@ class ExpressionParserTest {
 
     @ParameterizedTest
     @MethodSource("fixtures")
-    void test(final Fixture fixture) {
-        final ExpressionNode node = new ExpressionParser(fixture.input).parse();
+    void test(final String input, final ExpressionNode expectedNode, final int expectedValue) {
+        final ExpressionNode node = new ExpressionParser(input).parse();
         final ExpressionEvaluator evaluator = new ExpressionEvaluator();
         assertAll(() -> {
-            assertEquals(fixture.expectedNode, node);
-            assertEquals(fixture.expectedValue, evaluator.evaluate(node));
+            assertEquals(expectedNode, node);
+            assertEquals(expectedValue, evaluator.evaluate(node));
         });
     }
 
@@ -28,40 +29,22 @@ class ExpressionParserTest {
         assertThrows(ParseException.class, new ExpressionParser(input)::parse);
     }
 
-    static Stream<Fixture> fixtures() {
+    static Stream<Arguments> fixtures() {
         return Stream.of(
-                new Fixture("0", val(0), 0),
-                new Fixture("1", val(1), 1),
-                new Fixture("234567890", val(234567890), 234567890),
-                new Fixture("1+2+3+4", add(add(add(val(1), val(2)), val(3)), val(4)),
+                Arguments.of("0", val(0), 0),
+                Arguments.of("1", val(1), 1),
+                Arguments.of("234567890", val(234567890), 234567890),
+                Arguments.of("1+2+3+4", add(add(add(val(1), val(2)), val(3)), val(4)),
                         1 + 2 + 3 + 4),
-                new Fixture("9-1-2-3", sub(sub(sub(val(9), val(1)), val(2)), val(3)),
+                Arguments.of("9-1-2-3", sub(sub(sub(val(9), val(1)), val(2)), val(3)),
                         9 - 1 - 2 - 3),
-                new Fixture("1*2*3*4", mul(mul(mul(val(1), val(2)), val(3)), val(4)),
+                Arguments.of("1*2*3*4", mul(mul(mul(val(1), val(2)), val(3)), val(4)),
                         1 * 2 * 3 * 4),
-                new Fixture("210/5/3/2", div(div(div(val(210), val(5)), val(3)), val(2)),
+                Arguments.of("210/5/3/2", div(div(div(val(210), val(5)), val(3)), val(2)),
                         210 / 5 / 3 / 2),
-                new Fixture("1+2*3+4", add(add(val(1), mul(val(2), val(3))), val(4)),
+                Arguments.of("1+2*3+4", add(add(val(1), mul(val(2), val(3))), val(4)),
                         1 + 2 * 3 + 4),
-                new Fixture("(1+2)*(3+4)", mul(add(val(1), val(2)), add(val(3), val(4))),
+                Arguments.of("(1+2)*(3+4)", mul(add(val(1), val(2)), add(val(3), val(4))),
                         (1 + 2) * (3 + 4)));
-    }
-
-    static class Fixture {
-
-        final String input;
-        final ExpressionNode expectedNode;
-        final int expectedValue;
-
-        Fixture(final String input, final ExpressionNode expectedNode, final int expectedValue) {
-            this.input = input;
-            this.expectedNode = expectedNode;
-            this.expectedValue = expectedValue;
-        }
-
-        @Override
-        public String toString() {
-            return input;
-        }
     }
 }
