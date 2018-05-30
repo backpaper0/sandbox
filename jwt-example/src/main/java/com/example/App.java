f58498ab50ad681e25174094bc257c705ff992da
Taichi Uragami <backpaper0@gmail.com>
2018-05-31 07:09:35 +0900

    JWTの例

---
+++ jwt-example/src/main/java/com/example/App.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class App {
+
+    public static void main(final String[] args) throws Exception {
+
+        final KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        final KeyPair keyPair = generator.generateKeyPair();
+
+        final String token = Jwts.builder()
+                .setSubject("hello world")
+                .signWith(SignatureAlgorithm.RS256, keyPair.getPrivate())
+                .compact();
+
+        System.out.println(token);
+
+        final String subject = Jwts.parser()
+                .setSigningKey(keyPair.getPublic())
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        System.out.println(subject);
+    }
+}
