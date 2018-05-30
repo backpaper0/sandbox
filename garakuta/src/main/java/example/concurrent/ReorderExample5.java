fc5b128264cb818d2f90911c519f647ae1e4a884
Taichi Uragami <backpaper0@gmail.com>
2018-05-30 21:11:02 +0900

    ( ・ิω・ิ)

---
+++ garakuta/src/main/java/example/concurrent/ReorderExample5.java
@@ -0,0 +1,51 @@
+package example.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReorderExample5 implements Runnable {
+
+    public static void main(final String[] args) throws Exception {
+        final ReorderExample5 r = new ReorderExample5();
+        final Thread t = new Thread(r);
+        t.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        r.stop();
+    }
+
+    private boolean alive = true;
+    private final ReadWriteLock rwl = new ReentrantReadWriteLock();
+
+    @Override
+    public void run() {
+        long counter = 0L;
+        while (isAlive()) {
+            counter++;
+        }
+        System.out.printf("%,d", counter);
+    }
+
+    private boolean isAlive() {
+        final Lock lock = rwl.readLock();
+        lock.lock();
+        try {
+            return alive;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void stop() {
+        System.out.println("stop");
+        final Lock lock = rwl.writeLock();
+        lock.lock();
+        try {
+            alive = false;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ garakuta/src/main/java/example/concurrent/ReorderExample6.java
@@ -0,0 +1,56 @@
+package example.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.StampedLock;
+
+public class ReorderExample6 implements Runnable {
+
+    public static void main(final String[] args) throws Exception {
+        final ReorderExample6 r = new ReorderExample6();
+        final Thread t = new Thread(r);
+        t.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        r.stop();
+    }
+
+    private boolean alive = true;
+    private final StampedLock lock = new StampedLock();
+
+    @Override
+    public void run() {
+        long counter = 0L;
+        while (isAlive()) {
+            counter++;
+        }
+        System.out.printf("%,d", counter);
+    }
+
+    private boolean isAlive() {
+        //スタンプを取得して処理を行う
+        long stamp = lock.tryOptimisticRead();
+        boolean b = alive;
+        //スタンプの取得からここまでで書き込みロックが取得されていないか検証する
+        if (lock.validate(stamp) == false) {
+            //書き込みロックが取得されていた場合は読み取りロックを取得して再度処理を行う
+            stamp = lock.readLock();
+            try {
+                b = alive;
+            } finally {
+                lock.unlockRead(stamp);
+            }
+        }
+        return b;
+    }
+
+    public void stop() {
+        System.out.println("stop");
+        //書き込みロックを取得して処理を行う
+        final long stamp = lock.writeLock();
+        try {
+            alive = false;
+        } finally {
+            lock.unlockWrite(stamp);
+        }
+    }
+}
