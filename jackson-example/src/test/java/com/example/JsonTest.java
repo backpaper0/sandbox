8035763da0195446d9564b91b79b1fe90909bf17
Taichi Uragami <backpaper0@gmail.com>
2018-07-27 06:49:20 +0900

    JSON繰り返し

--- jackson-example/src/test/java/com/example/JsonTest.java
+++ jackson-example/src/test/java/com/example/JsonTest.java
@@ -2,9 +2,12 @@ package com.example;
 
 import static org.junit.Assert.*;
 
+import java.io.StringWriter;
+
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SequenceWriter;
 
 public class JsonTest {
 
@@ -14,5 +17,24 @@ public class JsonTest {
         final String json = mapper.writeValueAsString(new Message(123, "foobar"));
         System.out.println(json);
         assertEquals("{\"id\":123,\"content\":\"foobar\"}", json);
+    }
+
+    @Test
+    public void testIteration() throws Exception {
+        final ObjectMapper mapper = new ObjectMapper();
+
+        final StringWriter out = new StringWriter();
+        try (final SequenceWriter writer = mapper.writer().writeValues(out)) {
+            writer.init(true);
+            writer.write(new Message(123, "foo"));
+            writer.write(new Message(456, "bar"));
+            writer.write(new Message(789, "baz"));
+        }
+
+        final String json = out.toString();
+        System.out.println(json);
+        assertEquals(
+                "[{\"id\":123,\"content\":\"foo\"},{\"id\":456,\"content\":\"bar\"},{\"id\":789,\"content\":\"baz\"}]",
+                json);
     }
 }
