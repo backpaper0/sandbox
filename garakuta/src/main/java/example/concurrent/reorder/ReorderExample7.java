0d92db6aa4a2e03a8db4a99c033f86aebc09727e
Taichi Uragami <backpaper0@gmail.com>
2018-08-09 06:01:43 +0900

    (/・ω・)/

---
+++ garakuta/src/main/java/example/concurrent/reorder/ReorderExample7.java
@@ -0,0 +1,38 @@
+package example.concurrent.reorder;
+
+import java.util.concurrent.TimeUnit;
+
+public class ReorderExample7 implements Runnable {
+
+    public static void main(final String[] args) throws Exception {
+        final ReorderExample7 r = new ReorderExample7();
+        final Thread t = new Thread(r);
+        t.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        r.stop();
+    }
+
+    private boolean alive = true;
+    private volatile String s = "";
+
+    @Override
+    public void run() {
+        long counter = 0L;
+        while (isAlive()) {
+            counter++;
+        }
+        System.out.printf("%,d", counter);
+    }
+
+    private boolean isAlive() {
+        s.length();
+        return alive;
+    }
+
+    public void stop() {
+        s.length();
+        alive = false;
+        System.out.println("stop");
+    }
+}
---
+++ garakuta/src/main/java/example/concurrent/reorder/ReorderExample8.java
@@ -0,0 +1,38 @@
+package example.concurrent.reorder;
+
+import java.util.concurrent.TimeUnit;
+
+public class ReorderExample8 implements Runnable {
+
+    public static void main(final String[] args) throws Exception {
+        final ReorderExample8 r = new ReorderExample8();
+        final Thread t = new Thread(r);
+        t.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        r.stop();
+    }
+
+    private boolean alive = true;
+    private final StringBuffer buf = new StringBuffer();
+
+    @Override
+    public void run() {
+        long counter = 0L;
+        while (isAlive()) {
+            counter++;
+        }
+        System.out.printf("%,d", counter);
+    }
+
+    private boolean isAlive() {
+        buf.length();
+        return alive;
+    }
+
+    public void stop() {
+        buf.length();
+        alive = false;
+        System.out.println("stop");
+    }
+}
