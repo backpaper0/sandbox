bc4428abd4824cd53b2963a692ef23ecce2d38dd
Taichi Uragami <backpaper0@gmail.com>
2018-10-10 06:37:54 +0900

    jsを別ファイルに分けた

---
+++ scripts/breakout/index.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+//ctx.beginPath();
+//ctx.rect(20, 40, 50, 50);
+//ctx.fillStyle = "#FF0000";
+//ctx.fill();
+//ctx.closePath();
+//
+//ctx.beginPath();
+//ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+//ctx.fillStyle = "green";
+//ctx.fill();
+//ctx.closePath();
+//
+//ctx.beginPath();
+//ctx.rect(160, 10, 100, 40);
+//ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+//ctx.stroke();
+//ctx.closePath();
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
+//描画ループ
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+};
+setInterval(draw, 10);
