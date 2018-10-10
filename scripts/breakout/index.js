fd2a87d114e6b74ce311cc775bd424d5c620edb8
Taichi Uragami <backpaper0@gmail.com>
2018-10-11 04:57:16 +0900

    パドルとキーボード操作

--- scripts/breakout/index.js
+++ scripts/breakout/index.js
@@ -33,10 +33,47 @@ const drawBall = () => {
   ctx.closePath();
 };
 
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
+let rightPressed = false;
+let leftPressed = false;
+
+const keyDownHandler = e => {
+  if(e.keyCode == 39) {
+    rightPressed = true;
+  } else if(e.keyCode == 37) {
+    leftPressed = true;
+  }
+};
+
+const keyUpHandler = e => {
+  if(e.keyCode == 39) {
+    rightPressed = false;
+  } else if(e.keyCode == 37) {
+    leftPressed = false;
+  }
+};
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 //描画ループ
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
+
+  //ボール
   x += dx;
   y += dy;
 
@@ -46,6 +83,13 @@ const draw = () => {
 
   if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
+  }
+
+  //パドル
+  if(rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
   }
 };
 
