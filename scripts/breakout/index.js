8c3f6d16664f11f024749c82604e15f133a7bac7
Taichi Uragami <backpaper0@gmail.com>
2018-10-11 05:22:28 +0900

    ブロックのかたまりと衝突検出

--- scripts/breakout/index.js
+++ scripts/breakout/index.js
@@ -67,11 +67,61 @@ const keyUpHandler = e => {
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for(let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+const drawBricks = () => {
+  for(let c = 0; c < brickColumnCount; c++) {
+    for(let r = 0; r < brickRowCount; r++) {
+      if(bricks[c][r].status == 1) {
+        let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+};
+
+const collisionDetection = () => {
+  for(let c = 0; c < brickColumnCount; c++) {
+    for(let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+      if(b.status == 1) {
+        if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
+  }
+};
+
 //描画ループ
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
+  collisionDetection(); 
+  drawBricks();
 
   //ボール
   x += dx;
