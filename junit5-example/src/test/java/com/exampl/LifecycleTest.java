e92bed249946526e0e058ec4f6b3522f69a23dac
Taichi Uragami <backpaper0@gmail.com>
2019-01-14 13:25:50 +0900

    Add JUnit5 example

---
+++ junit5-example/src/test/java/com/exampl/LifecycleTest.java
@@ -0,0 +1,47 @@
+package com.exampl;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class LifecycleTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        System.out.println("setUpAll");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("setUp");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("test2");
+    }
+
+    @Test
+    @Disabled
+    void test3() {
+        System.out.println("test3");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("tearDown");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("tearDownAll");
+    }
+}
