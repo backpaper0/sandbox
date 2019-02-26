d263ee9f6c4dc03fa910167e72f35a91e78697eb
Taichi Uragami <backpaper0@gmail.com>
2019-02-27 06:03:38 +0900

    Spring WebFluxにした

--- nginx-reverse-proxy-example/api/src/main/java/com/example/api/ApiApplication.java
+++ nginx-reverse-proxy-example/api/src/main/java/com/example/api/ApiApplication.java
@@ -1,34 +1,37 @@
 package com.example.api;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.time.Duration;
+import java.util.stream.Collectors;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
-@SpringBootApplication
-@RestController
+import reactor.core.publisher.Mono;
+import reactor.netty.http.server.HttpServer;
+
 public class ApiApplication {
 
     public static void main(final String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        final RouterFunction<ServerResponse> routerFunction = RouterFunctions.route()
+                .GET("/hello", ApiApplication::getHello)
+                .build();
+        final HttpHandler httpHandler = RouterFunctions.toHttpHandler(routerFunction);
+        final HttpServer server = HttpServer.create().host("0.0.0.0").port(8080)
+                .handle(new ReactorHttpHandlerAdapter(httpHandler));
+        server.bindUntilJavaShutdown(Duration.ofMinutes(1), a -> {
+        });
     }
 
-    @RequestMapping(path = "/hello", produces = "text/plain")
-    public String hello(@RequestHeader final HttpHeaders httpHeaders) {
-        final StringWriter sw = new StringWriter();
-        try (PrintWriter out = new PrintWriter(sw)) {
-            httpHeaders.forEach((key, values) -> {
-                out.printf("%s%n", key);
-                values.forEach(value -> out.printf("    %s%n", value));
-            });
-            out.flush();
-        }
-        return sw.toString();
+    static Mono<ServerResponse> getHello(final ServerRequest request) {
+        final HttpHeaders headers = request.headers().asHttpHeaders();
+        final String s = headers.entrySet().stream().map(a -> a.getKey() + "=" + a.getValue())
+                .collect(Collectors.joining("\n", "", "\n"));
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).syncBody(s);
     }
-
 }
