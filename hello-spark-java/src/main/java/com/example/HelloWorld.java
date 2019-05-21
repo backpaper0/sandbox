9041b2015215c6111bf0b9d63f811ecbdbeb07e5
Taichi Uragami <backpaper0@gmail.com>
2019-05-22 06:37:41 +0900

    特に興味なし

--- hello-spark-java/src/main/java/com/example/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import static spark.Spark.*;
-
-public class HelloWorld {
-    public static void main(final String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
-}
