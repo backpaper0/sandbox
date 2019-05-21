3a6a21b91012319144b6b9636e3d012ed6c9c053
Taichi Uragami <backpaper0@gmail.com>
2019-05-22 06:39:36 +0900

    JavaFX使うことはないかな……

--- javafx-guice-sample/src/main/java/sample/Hello.java
+++
@@ -1,6 +0,0 @@
-package sample;
-
-public interface Hello {
-
-    String greet(String yourName);
-}
--- javafx-guice-sample/src/main/java/sample/HelloImpl.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-public class HelloImpl implements Hello {
-
-    @Override
-    public String greet(String yourName) {
-        return String.format("Hello, %s!", yourName);
-    }
-}
