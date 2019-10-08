761ad8fc4f8882fc44b0e6f441bb6f940595b2ec
Uragami Taichi <backpaper0@gmail.com>
2019-10-09 06:52:52 +0900

    Replace var to const, function literal to arrow funciton

--- express-mongodb-example/app.js
+++ express-mongodb-example/app.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var tasksRouter = require('./routes/tasks');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
 
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/example', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('debug', true);
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
--- express-mongodb-example/routes/tasks.js
+++ express-mongodb-example/routes/tasks.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const mongoose = require('mongoose');
 const Task = require("../models/Task");
 
-router.post('/', async function(req, res, next) {
+router.post('/', async (req, res, next) => {
 
 	const title = req.body.title;
 	const done = false;
