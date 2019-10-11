74bde15ea4f2cde59da6c307c571e030edfca143
Uragami Taichi <backpaper0@gmail.com>
2019-10-11 14:48:57 +0900

    get

--- express-mongodb-example/routes/tasks.js
+++ express-mongodb-example/routes/tasks.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Task = require("../models/Task");
 
+router.get('/:taskId', async (req, res, next) => {
+
+	const { taskId } = req.params;
+	const task = await Task.findById(taskId).exec();
+
+	res.send(task);
+});
+
 router.post('/', async (req, res, next) => {
 
 	const title = req.body.title;
