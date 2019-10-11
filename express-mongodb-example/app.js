0eae917785b18594137a68789095edddb070aaa2
Uragami Taichi <backpaper0@gmail.com>
2019-10-11 17:48:30 +0900

    Authn

--- express-mongodb-example/app.js
+++ express-mongodb-example/app.js
@@ -7,6 +7,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const tasksRouter = require('./routes/tasks');
 const sessionsRouter = require('./routes/sessions');
+const authRouter = require('./routes/auth');
 
 const session = require('express-session');
 const redis = require('redis');
@@ -15,6 +16,27 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/example', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('debug', true);
+
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    if (username === "demo" && password === "secret") {
+			done(null, { username });
+		} else {
+		  done(null, false, { message: "Login failure" });
+		}
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, { username: id });
+});
 
 const RedisStore = require('connect-redis')(session)
 const client = redis.createClient()
@@ -27,10 +49,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ store: new RedisStore({ client }), secret: "keyboard cat", resave: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 app.use('/sessions', sessionsRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
