2b97e16ec7cd2e33c83063b3a77a25045ca4b307
Uragami Taichi <backpaper0@gmail.com>
2019-10-16 21:48:28 +0900

    Error handling

--- hello-express/routes/index.js
+++ hello-express/routes/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
   const { a } = req.body;
-  if (a === "throw") {
-    throw "thrown";
-  } else if (a === "error-async") {
-    setTimeout(() => next("error-async"), 100);
+  if (a === "1") {
+    throw "1";
+  } else if (a === "2") {
+    next("2");
+  } else if (a === "3") {
+    setTimeout(() => next("3"), 100);
   } else {
     res.send("Hello, world!");
   }
