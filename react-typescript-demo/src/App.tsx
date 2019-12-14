80aff0cd94ee7aee2233cb204c98b04da2f6de99
Uragami Taichi <backpaper0@gmail.com>
2019-12-14 10:53:48 +0900

    Hello, React + TypeScript!

---
+++ react-typescript-demo/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const App: React.FC = () => {
+	// useStateの戻り値は配列の要素毎に型が明示されているので
+	// 代入するときに型推論されるっぽい
+	// https://github.com/facebook/react/blob/v16.12.0/packages/react-reconciler/src/ReactFiberHooks.js#L65
+	const [count, setCount] = useState(0);
+	test1(count);
+	test2(setCount);
+	return (
+		<div>
+			<p>You clicked {count} times</p>
+			<button onClick={() => setCount(count + 1)}>
+				Click me
+			</button>
+		</div>
+	);
+}
+
+function test1(count: number) {
+}
+
+function test2(setCount: (count: number) => void) {
+}
+
+export default App;
