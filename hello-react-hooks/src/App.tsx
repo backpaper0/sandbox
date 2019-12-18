09a57ef5a2e0070ce36fc103a1ce2975630a02a6
Uragami Taichi <backpaper0@gmail.com>
2019-12-18 22:55:18 +0900

    Update

--- hello-react-hooks/src/App.tsx
+++ hello-react-hooks/src/App.tsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
-  const countUp = () => setCount(count + 1);
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
-  return (
-    <div>
-      <button onClick={countUp}>increase</button>
-      <div>count: {count}</div>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	const [isLoading, setLoading] = useState(false);
+	const countUp = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+			setCount(count => count + 1);
+		}, 200);
+	};
+	useEffect(() => {
+		document.title = `You clicked ${count} times`;
+	});
+	return (
+		<div>
+			<button onClick={countUp} disabled={isLoading}>
+				{isLoading ? "loading..." : "increase"}
+			</button>
+			<div>count: {count}</div>
+		</div>
+	);
 }
 
 export default App;
