7924ffdea3af78a6c768e295c90c2970ceb5be9a
Uragami Taichi <backpaper0@gmail.com>
2020-02-05 22:38:58 +0900

    TomcatとWebSocketで遊ぶ

---
+++ websocket-tomcat-example/websocket-tomcat-example-server/src/main/java/com/example/App.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+public class App {
+
+    public static void main(final String[] args) throws Exception {
+
+        final Tomcat tomcat = new Tomcat();
+        //        tomcat.setHostname("0.0.0.0");
+        tomcat.setPort(8080);
+
+        final Context context = tomcat.addWebapp("", Files
+                .createDirectories(Paths.get("target/docbase")).toAbsolutePath().toString());
+
+        final WebResourceRoot resources = new StandardRoot(context);
+
+        final DirResourceSet webResourceSet = new DirResourceSet();
+        webResourceSet.setWebAppMount("/WEB-INF/classes");
+        webResourceSet.setBase(Paths.get("target/classes").toAbsolutePath().toString());
+
+        resources.addPreResources(webResourceSet);
+        context.setResources(resources);
+
+        tomcat.start();
+
+        System.out.println("Tomcat started");
+
+        TimeUnit.DAYS.sleep(1);
+    }
+}
---
+++ websocket-tomcat-example/websocket-tomcat-example-server/src/main/java/com/example/MessagingEndPoint.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/messaging")
+public class MessagingEndPoint {
+
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    @OnOpen
+    public void onOpen(final Session session) {
+        System.out.println("[open] " + session);
+        final ScheduledFuture<?> future = executor.scheduleAtFixedRate(() -> {
+            try {
+                session.getBasicRemote().sendText(LocalDateTime.now().toString());
+            } catch (final IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }, 0, 5, TimeUnit.SECONDS);
+        session.getUserProperties().put("future", future);
+    }
+
+    @OnMessage
+    public String onMessage(final String message, final Session session) {
+        System.out.println("[" + message + "] " + session);
+        return message;
+    }
+
+    @OnClose
+    public void onClose(final Session session) {
+        System.out.println("[close] " + session);
+        final ScheduledFuture<?> future = (ScheduledFuture<?>) session.getUserProperties()
+                .get("future");
+        future.cancel(false);
+    }
+}
