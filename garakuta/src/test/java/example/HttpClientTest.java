d7fe6b7ea3f127d6b26c57efa9ced9d64bd239a6
Uragami Taichi <backpaper0@gmail.com>
2020-02-13 06:37:40 +0900

    JSON

--- garakuta/src/test/java/example/HttpClientTest.java
+++ garakuta/src/test/java/example/HttpClientTest.java
@@ -2,13 +2,17 @@ package example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandler;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.nio.charset.Charset;
+import java.net.http.HttpResponse.BodySubscriber;
+import java.net.http.HttpResponse.BodySubscribers;
+import java.net.http.HttpResponse.ResponseInfo;
+import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,16 +20,23 @@ import org.mockserver.client.MockServerClient;
 import org.mockserver.junit.jupiter.MockServerExtension;
 import org.mockserver.junit.jupiter.MockServerSettings;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @ExtendWith(MockServerExtension.class)
 @MockServerSettings(ports = 8080)
 class HttpClientTest {
+
+    String json = "{\"message\":\"Hello, world!\"}";
 
     @Test
     void test(final MockServerClient client) throws Exception {
 
         client.when(org.mockserver.model.HttpRequest.request("/example")
                 .withHeader("X-Request-Id", "xyz"))
-                .respond(org.mockserver.model.HttpResponse.response("Hello, world!"));
+                .respond(org.mockserver.model.HttpResponse
+                        .response("{\"message\":\"Hello, world!\"}"));
 
         final HttpClient http = HttpClient.newHttpClient();
 
@@ -35,11 +46,74 @@ class HttpClientTest {
                 .header("X-Request-Id", "xyz")
                 .build();
 
-        final BodyHandler<String> responseBodyHandler = BodyHandlers
-                .ofString(Charset.forName("UTF-8"));
+        final BodyHandler<Model> responseBodyHandler = JsonBodyHandler.get(Model.class);
 
-        final HttpResponse<String> response = http.send(request, responseBodyHandler);
+        final HttpResponse<Model> response = http.send(request, responseBodyHandler);
 
-        assertEquals("Hello, world!", response.body());
+        assertEquals(new Model("Hello, world!"), response.body());
+    }
+
+    public static class JsonBodyHandler<T> implements BodyHandler<T> {
+
+        private static ObjectMapper defaultObjectMapper = new ObjectMapper();
+        private final ObjectMapper objectMapper;
+        private final Class<T> clazz;
+
+        public JsonBodyHandler(final ObjectMapper om, final Class<T> clazz) {
+            this.objectMapper = om;
+            this.clazz = clazz;
+        }
+
+        public static <T> BodyHandler<T> get(final Class<T> clazz) {
+            return new JsonBodyHandler<>(defaultObjectMapper, clazz);
+        }
+
+        @Override
+        public BodySubscriber<T> apply(final ResponseInfo responseInfo) {
+            return BodySubscribers
+                    .mapping(BodySubscribers.ofByteArray(), bytes -> {
+                        try {
+                            return objectMapper.readValue(bytes, clazz);
+                        } catch (final IOException e) {
+                            throw new UncheckedIOException(e);
+                        }
+                    });
+        }
+    }
+
+    public static class Model {
+
+        private final String message;
+
+        @JsonCreator
+        public Model(@JsonProperty("message") final String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(message);
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            final Model other = (Model) obj;
+            return message.equals(other.message);
+        }
+
+        @Override
+        public String toString() {
+            return "Model [message=" + message + "]";
+        }
     }
 }
