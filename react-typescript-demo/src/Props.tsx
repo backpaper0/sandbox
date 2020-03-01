09fe17ca0bf08030fed4afc95959188432916d95
Uragami Taichi <backpaper0@gmail.com>
2020-03-02 07:09:07 +0900

    これもType Aliasでも良さそう

--- react-typescript-demo/src/Props.tsx
+++ react-typescript-demo/src/Props.tsx
@@ -12,9 +12,23 @@ interface DemoProps {
   baz: boolean;
 }
 
+const Demo2: React.FC<Demo2Props> = ({ foo, bar, baz }) => {
+  return (
+    <li>{`${foo} ${bar} ${baz}`}</li>
+  );
+};
+
+//Type Aliasでもよさそう
+type Demo2Props = {
+  foo: string;
+  bar: number;
+  baz: boolean;
+};
+
 export default () => (
   <ul>
     <Demo foo="abc" bar={123} baz={true}/>
     <Demo foo="def" bar={456} baz={false}/>
+    <Demo2 foo="ghi" bar={789} baz={true}/>
   </ul>
 );
