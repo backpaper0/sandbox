3736ed5cfe361a62e39f781e2900e0cc411a9a8d
Uragami Taichi <backpaper0@gmail.com>
2020-03-02 21:28:27 +0900

    Polish

--- react-example/src/SplashExample.tsx
+++ react-example/src/SplashExample.tsx
@@ -4,16 +4,14 @@ import splash from "./splash.png";
 
 export default () => {
   const [initialized, setInitialized] = useState(false);
-  useEffect(() => {
-    setTimeout(() => setInitialized(true), 2000);
-  }, []);
+  const callback = () => setInitialized(true);
   if (initialized) {
     return (
       <App/>
     );
   }
   return (
-    <Splash/>
+    <Splash callback={callback}/>
   );
 };
 
@@ -23,19 +21,27 @@ const App = () => (
   </div>
 );
 
-const Splash = () => (
-  <FadeOut init={{ opacity: 1 }} transition={{ opacity: 0, transition: "opacity 1000ms" }} delay={1000}>
+const Splash: React.FC<{ callback: () => void; }> = ({ callback }) => (
+  <FadeOut init={{ opacity: 1 }} transition={{ opacity: 0, transition: "opacity 1500ms" }} delay={1500} callback={callback}>
     <img src={splash}/>
   </FadeOut>
 );
 
-const FadeOut: React.FC<{ init: any, transition: any, delay: number, children: any }> = ({ init, transition, delay, children }) => {
+type FadeOutProps = {
+  init: any;
+  transition: any;
+  delay: number;
+  children: any;
+  callback: () => void;
+}
+
+const FadeOut: React.FC<FadeOutProps> = ({ init, transition, delay, children, callback }) => {
   const [begin, setBegin] = useState(false);
   useEffect(() => {
     setTimeout(() => setBegin(true), delay);
   }, []);
   return (
-    <div style={ begin ? transition : init }>
+    <div style={ begin ? transition : init } onTransitionEnd={callback}>
       {children}
     </div>
   );
