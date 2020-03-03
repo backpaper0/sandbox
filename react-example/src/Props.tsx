c36aa824679683d7b4e1a2421179d2f493aed302
Uragami Taichi <backpaper0@gmail.com>
2020-03-03 18:56:04 +0900

    匿名(?)でも型を書けそう

--- react-example/src/Props.tsx
+++ react-example/src/Props.tsx
@@ -25,10 +25,18 @@ type Demo2Props = {
   baz: boolean;
 };
 
+//匿名(?)でも型を書けそう
+const Demo3: React.FC<{ foo: string; bar: number; baz: boolean; }> = ({ foo, bar, baz }) => {
+  return (
+    <li>{`${foo} ${bar} ${baz}`}</li>
+  );
+};
+
 export default () => (
   <ul>
     <Demo foo="abc" bar={123} baz={true}/>
     <Demo foo="def" bar={456} baz={false}/>
     <Demo2 foo="ghi" bar={789} baz={true}/>
+    <Demo3 foo="jkl" bar={876} baz={false}/>
   </ul>
 );
