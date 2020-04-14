7d0e120c5bc60d5d66999435a1006dedbf6e24fa
Uragami Taichi <backpaper0@gmail.com>
2020-04-15 06:40:09 +0900

    アノテーションの実体を忘れてた

--- garakuta/src/main/java/example/classclass/ClassClassExample.java
+++ garakuta/src/main/java/example/classclass/ClassClassExample.java
@@ -1,12 +1,17 @@
 package example.classclass;
 
+import java.lang.reflect.Proxy;
+
 public class ClassClassExample {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws Exception {
         p("クラス", ExampleClass.class);
         p("抽象クラス", ExampleAbstractClass.class);
         p("インターフェース", ExampleInterface.class);
         p("アノテーション", ExampleAnnotation.class);
+        p("アノテーション(実体)",
+                ClassClassExample.class.getDeclaredField("a").getAnnotation(ExampleAnnotation.class)
+                        .getClass());
         p("列挙型", ExampleEnum.class);
         p("列挙型(値)", ExampleEnum.VALUE1.getClass());
         p("列挙型(値,メソッドオーバーライド)", ExampleEnum.VALUE2.getClass());
@@ -16,12 +21,12 @@ public class ClassClassExample {
     Object a;
 
     private static void p(final String desc, final Class<?> clazz) {
-        System.out.printf("# %s%n", desc);
-        System.out.printf("        クラス名: %s%n", clazz.getName());
-        System.out.printf("    isAnnotation: %s%n", clazz.isAnnotation());
-        System.out.printf("isAnonymousClass: %s%n", clazz.isAnonymousClass());
-        System.out.printf("          isEnum: %s%n", clazz.isEnum());
-        System.out.printf("     isInterface: %s%n", clazz.isInterface());
+        System.out.printf("# %s - %s%n", desc, clazz.getName());
+        System.out.printf("    Class.isAnnotation: %s%n", clazz.isAnnotation());
+        System.out.printf("Class.isAnonymousClass: %s%n", clazz.isAnonymousClass());
+        System.out.printf("          Class.isEnum: %s%n", clazz.isEnum());
+        System.out.printf("     Class.isInterface: %s%n", clazz.isInterface());
+        System.out.printf("    Proxy.isProxyClass: %s%n", Proxy.isProxyClass(clazz));
         System.out.printf("%n");
     }
 }
