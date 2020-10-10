82d13046dda5a0ea4b990fa6bc7c8ed13e503ae3
Uragami Taichi <backpaper0@gmail.com>
2020-10-10 10:37:18 +0900

    もう要らないかなーと思ったものを削除

--- deltaspike-data-sample/src/main/java/sample/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-import org.apache.deltaspike.data.api.EntityRepository;
-import org.apache.deltaspike.data.api.Repository;
-
-@Repository
-public interface AccountRepository extends EntityRepository<Account, Long> {
-}
