471159a4eec3f301d82b777e08358fdc9a2fd448
Uragami Taichi <backpaper0@gmail.com>
2021-03-27 22:59:50 +0900

    panic!じゃなくてResultにした

--- rust-examples/calc/src/lib.rs
+++ rust-examples/calc/src/lib.rs
@@ -32,7 +32,7 @@ impl<'a> Lexer<'a> {
         self.c = self.source.next();
     }
 
-    fn next(&mut self) -> Token {
+    fn next(&mut self) -> Result<Token, String> {
         while let Some(c) = self.c {
             match c {
                 ' ' => {
@@ -53,18 +53,18 @@ impl<'a> Lexer<'a> {
                             },
                         }
                     }
-                    return Token(TokenType::Num, s)
+                    return Ok(Token(TokenType::Num, s));
                 },
                 '+' => {
                     self.consume();
-                    return Token(TokenType::Add, String::from("+"))
+                    return Ok(Token(TokenType::Add, String::from("+")))
                 },
                 _ => {
-                    panic!("Illegal character: {}", c);
+                    return Err(format!("Illegal character: {}", c));
                 }
             }
         }
-        Token(TokenType::Eof, String::new())
+        Ok(Token(TokenType::Eof, String::new()))
     }
 }
 
@@ -81,46 +81,49 @@ pub struct Parser<'a> {
 
 impl<'a> Parser<'a> {
     
-    pub fn new(source: &'a String) -> Parser<'a> {
+    pub fn new(source: &'a String) -> Result<Parser<'a>, String> {
         let mut lexer = Lexer::new(source);
-        let token = lexer.next();
-        Parser {
+        let token = lexer.next()?;
+        Ok(Parser {
             lexer,
             token,
+        })
+    }
+
+    fn consume(&mut self) -> Result<(), String> {
+        self.token = self.lexer.next()?;
+        Ok(())
+    }
+
+    fn expect(&mut self, token_type: TokenType) -> Result<(), String> {
+        if self.token.0 == token_type {
+            Ok(())
+        } else {
+            Err(format!("Unexpected token: expected = {:?}, actual = {:?}", token_type, self.token))
         }
     }
 
-    fn consume(&mut self) {
-        self.token = self.lexer.next();
-    }
-
-    fn expect(&mut self, token_type: TokenType) {
-        if self.token.0 != token_type {
-            panic!("Unexpected token: expected = {:?}, actual = {:?}", token_type, self.token);
-        }
-    }
-
-    pub fn parse(&mut self) -> Node {
-        let node = self.parse_add();
-        self.expect(TokenType::Eof);
-        node
+    pub fn parse(&mut self) -> Result<Node, String> {
+        let node = self.parse_add()?;
+        self.expect(TokenType::Eof)?;
+        Ok(node)
     } 
 
-    fn parse_add(&mut self) -> Node {
-        let mut node = self.parse_num();
+    fn parse_add(&mut self) -> Result<Node, String> {
+        let mut node = self.parse_num()?;
         while self.token.0 == TokenType::Add {
-            self.consume();
-            let right = self.parse_num();
+            self.consume()?;
+            let right = self.parse_num()?;
             node = Node::Add(Box::new(node), Box::new(right))
         }
-        node
+        Ok(node)
     }
 
-    fn parse_num(&mut self) -> Node {
-        self.expect(TokenType::Num);
+    fn parse_num(&mut self) -> Result<Node, String> {
+        self.expect(TokenType::Num)?;
         let node = Node::Num(self.token.1.parse().unwrap());
-        self.consume();
-        node
+        self.consume()?;
+        Ok(node)
     }
 }
 
@@ -140,7 +143,7 @@ mod tests {
     fn next_eof() {
         let source = String::new();
         let mut sut = Lexer::new(&source);
-        let token = sut.next();
+        let token = sut.next().unwrap();
         assert_eq!(Token(TokenType::Eof, String::new()), token);
     }
 
@@ -148,7 +151,7 @@ mod tests {
     fn next_num_0() {
         let source = String::from("0");
         let mut sut = Lexer::new(&source);
-        let token = sut.next();
+        let token = sut.next().unwrap();
         assert_eq!(Token(TokenType::Num, String::from("0")), token);
     }
 
@@ -156,7 +159,7 @@ mod tests {
     fn next_num_12() {
         let source = String::from("12");
         let mut sut = Lexer::new(&source);
-        let token = sut.next();
+        let token = sut.next().unwrap();
         assert_eq!(Token(TokenType::Num, String::from("12")), token);
     }
 
@@ -164,7 +167,7 @@ mod tests {
     fn next_num_345() {
         let source = String::from("345");
         let mut sut = Lexer::new(&source);
-        let token = sut.next();
+        let token = sut.next().unwrap();
         assert_eq!(Token(TokenType::Num, String::from("345")), token);
     }
 
@@ -172,7 +175,7 @@ mod tests {
     fn next_num_6789() {
         let source = String::from("6789");
         let mut sut = Lexer::new(&source);
-        let token = sut.next();
+        let token = sut.next().unwrap();
         assert_eq!(Token(TokenType::Num, String::from("6789")), token);
     }
 
@@ -180,7 +183,7 @@ mod tests {
     fn next_add() {
         let source = String::from("+");
         let mut sut = Lexer::new(&source);
-        let token = sut.next();
+        let token = sut.next().unwrap();
         assert_eq!(Token(TokenType::Add, String::from("+")), token);
     }
 
@@ -189,37 +192,37 @@ mod tests {
     fn next_illegal_char() {
         let source = String::from("abc");
         let mut sut = Lexer::new(&source);
-        sut.next();
+        sut.next().unwrap();
     }
 
     #[test]
     fn next_combination() {
         let source = String::from("1234 + 567890");
         let mut sut = Lexer::new(&source);
-        assert_eq!(Token(TokenType::Num, String::from("1234")), sut.next());
-        assert_eq!(Token(TokenType::Add, String::from("+")), sut.next());
-        assert_eq!(Token(TokenType::Num, String::from("567890")), sut.next());
-        assert_eq!(Token(TokenType::Eof, String::from("")), sut.next());
+        assert_eq!(Token(TokenType::Num, String::from("1234")), sut.next().unwrap());
+        assert_eq!(Token(TokenType::Add, String::from("+")), sut.next().unwrap());
+        assert_eq!(Token(TokenType::Num, String::from("567890")), sut.next().unwrap());
+        assert_eq!(Token(TokenType::Eof, String::from("")), sut.next().unwrap());
     }
 
     #[test]
     fn parse_num() {
         let source = String::from("12345");
-        let mut sut = Parser::new(&source);
-        assert_eq!(Node::Num(12345), sut.parse());
+        let mut sut = Parser::new(&source).unwrap();
+        assert_eq!(Node::Num(12345), sut.parse().unwrap());
     }
 
     #[test]
     fn parse_num_add_num() {
         let source = String::from("678 + 90");
-        let mut sut = Parser::new(&source);
-        assert_eq!(Node::Add(Box::new(Node::Num(678)), Box::new(Node::Num(90))), sut.parse());
+        let mut sut = Parser::new(&source).unwrap();
+        assert_eq!(Node::Add(Box::new(Node::Num(678)), Box::new(Node::Num(90))), sut.parse().unwrap());
     }
 
     #[test]
     fn parse_num_add_num_repeat() {
         let source = String::from("1 + 23 + 456 + 7890");
-        let mut sut = Parser::new(&source);
+        let mut sut = Parser::new(&source).unwrap();
         let a = Box::new(Node::Num(1));
         let b = Box::new(Node::Num(23));
         let c = Box::new(Node::Num(456));
@@ -227,27 +230,27 @@ mod tests {
         let e = Box::new(Node::Add(a, b));
         let f = Box::new(Node::Add(e, c));
         let g = Node::Add(f, d);
-        assert_eq!(g, sut.parse());
+        assert_eq!(g, sut.parse().unwrap());
     }
 
     #[test]
     fn evaluate_num() {
         let source = String::from("12345");
-        let node = Parser::new(&source).parse();
+        let node = Parser::new(&source).unwrap().parse().unwrap();
         assert_eq!(12345, evaluate(node));
     }
 
     #[test]
     fn evaluate_num_add_num() {
         let source = String::from("678 + 90");
-        let node = Parser::new(&source).parse();
+        let node = Parser::new(&source).unwrap().parse().unwrap();
         assert_eq!(678 + 90, evaluate(node));
     }
 
     #[test]
     fn evaluate_num_add_num_repeat() {
         let source = String::from("1 + 23 + 456 + 7890");
-        let node = Parser::new(&source).parse();
+        let node = Parser::new(&source).unwrap().parse().unwrap();
         assert_eq!(1 + 23 + 456 + 7890, evaluate(node));
     }
 }
--- rust-examples/calc/src/main.rs
+++ rust-examples/calc/src/main.rs
@@ -1,15 +1,21 @@
 extern crate calc;
 
 use std::env;
+use std::process;
 use calc::Parser;
 
 fn main() {
     let mut args = env::args();
     args.next();
     let source = args.next().unwrap();
-    let mut parser = Parser::new(&source);
-    let node = parser.parse();
+    let mut parser = Parser::new(&source).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+    let node = parser.parse().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
     let result = calc::evaluate(node);
     println!("{}", result);
 }
-
