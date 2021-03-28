8dc79784e3a514408a25eb270892fdefeccd141b
Uragami Taichi <backpaper0@gmail.com>
2021-03-28 21:48:27 +0900

    Vecと所有権を理解したい

--- rust-examples/example/src/bin/vec.rs
+++ rust-examples/example/src/bin/vec.rs
@@ -8,6 +8,61 @@ fn main() {
     println!("{:?}", zs);
 
     println!("{}", sum(&xs));
+
+    //--------------------------------------------------
+    //Copyトレイトを実装しているi32のVec
+    let a = 1;
+    let xs = vec![a];
+    println!("{:?}", xs);
+
+    //Vec構築の際、所有権は移動されず値がコピーされるだけなので、ここでも使用できる
+    println!("{}", a);
+
+    //所有権を移動するのではなく、値がコピーされるだけ
+    let b = xs[0];
+    println!("{}", b);
+
+    for x in xs.iter() {
+        let y: &i32 = x;
+        println!("{}", y);
+    }
+    for x in &xs {
+        let y: &i32 = x;
+        println!("{}", y);
+    }
+    for x in xs { //所有権が移動するため、これ以降はxsは使えない
+        let y: i32 = x;
+        println!("{}", y);
+    }
+
+    //--------------------------------------------------
+    //Copyトレイトを実装していないStringのVec
+    let a = String::from("foo");
+    let xs = vec![a];
+    println!("{:?}", xs);
+
+    //所有権がxsへ移っているため、ここでは使用できない
+    //error[E0382]: borrow of moved value: `a`
+    //move occurs because `a` has type `String`, which does not implement the `Copy` trait
+    //println!("{}", a);
+
+    //所有権は移動できない
+    //error[E0507]: cannot move out of index of `Vec<String>`
+    //move occurs because value has type `String`, which does not implement the `Copy` trait
+    //let b = xs[0];
+
+    for x in xs.iter() {
+        let y: &String = x;
+        println!("{}", y);
+    }
+    for x in &xs {
+        let y: &String = x;
+        println!("{}", y);
+    }
+    for x in xs { //所有権が移動するため、これ以降はxsは使えない
+        let y: String = x;
+        println!("{}", y);
+    }
 }
 
 fn sum(xs: &[i32]) -> i32 {
