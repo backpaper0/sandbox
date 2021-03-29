93e78850f0e486963cf021b13459a6e42132649e
Uragami Taichi <backpaper0@gmail.com>
2021-03-29 09:15:20 +0900

    改善(になってるかな？)

--- rust-examples/example/src/bin/my_list.rs
+++ rust-examples/example/src/bin/my_list.rs
@@ -1,19 +1,30 @@
 fn main() {
-    assert_eq!(my_list(&vec![1, 2, 3]), List::Cons(1, Some(Box::new(List::Cons(2, Some(Box::new(List::Cons(3, Some(Box::new(List::Nil))))))))));
+    assert_eq!(List::new(&vec![1, 2, 3]), List {
+        elem: Elem::Cons(1, Some(Box::new(Elem::Cons(2, Some(Box::new(Elem::Cons(3, Some(Box::new(Elem::Nil)))))))))
+    });
 }
 
 #[derive(Debug, PartialEq)]
-enum List {
-    Cons(i32, Option<Box<List>>),
-    Nil,
+struct List {
+    elem: Elem,
 }
 
-fn my_list(xs: &[i32]) -> List {
-    let mut list = List::Nil;
-    for &x in xs.into_iter().rev() {
-        let tail = Some(Box::new(list));
-        list = List::Cons(x, tail);
+impl List {
+    fn new(xs: &[i32]) -> List {
+        let mut elem = Elem::Nil;
+        for &x in xs.into_iter().rev() {
+            let tail = Some(Box::new(elem));
+            elem = Elem::Cons(x, tail);
+        }
+        List {
+            elem
+        }
     }
-    list
+}
+
+#[derive(Debug, PartialEq)]
+enum Elem {
+    Cons(i32, Option<Box<Elem>>),
+    Nil,
 }
 
