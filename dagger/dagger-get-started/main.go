2b738f732c6762b4432d7093e8fabdd63a004252
Uragami Taichi <backpaper0@gmail.com>
2022-11-21 21:45:53 +0900

    キャッシュのキーはpom.xmlをSHA-1したものにする

--- dagger/dagger-get-started/main.go
+++ dagger/dagger-get-started/main.go
@@ -1,56 +1,66 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "os"
+	"context"
+	"fmt"
+	"os"
 
-    "dagger.io/dagger"
+	"crypto/sha1"
+	"dagger.io/dagger"
+	"io/ioutil"
 )
 
 func main() {
-    if err := build(context.Background()); err != nil {
-        fmt.Println(err)
-    }
+	if err := build(context.Background()); err != nil {
+		fmt.Println(err)
+	}
 }
 
 func build(ctx context.Context) error {
-    fmt.Println("Building with Dagger")
+	fmt.Println("Building with Dagger")
 
-    // initialize Dagger client
-    client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-    if err != nil {
-        return err
-    }
-    defer client.Close()
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
 
-    // get reference to the local project
-    src := client.Host().Workdir()
+	// get reference to the local project
+	src := client.Host().Workdir()
 
-	cacheKey := "m2-cache-v1"
+	pomXml, err := ioutil.ReadFile("pom.xml")
+	if err != nil {
+		panic(err)
+	}
+
+	hash := sha1.New()
+	hash.Write(pomXml)
+
+	cacheKey := fmt.Sprintf("%x", hash.Sum(nil))
 
 	cache := client.CacheVolume(cacheKey)
 
 	container := client.Container().From("bellsoft/liberica-openjdk-debian:17")
 
-    container = container.WithMountedDirectory("/src", src).WithWorkdir("/src")
+	container = container.WithMountedDirectory("/src", src).WithWorkdir("/src")
 
 	container = container.WithMountedCache("/m2", cache)
 
-    // define the application build command
-    path := "target/"
-    container = container.Exec(dagger.ContainerExecOpts{
+	// define the application build command
+	path := "target/"
+	container = container.Exec(dagger.ContainerExecOpts{
 		Args: []string{"./mvnw", "package", "-Dmaven.repo.local=/m2/repository", "--batch-mode", "--no-transfer-progress"},
-    })
+	})
 
-    // get reference to build output directory in container
-    output := container.Directory(path)
+	// get reference to build output directory in container
+	output := container.Directory(path)
 
-    // write contents of container build/ directory to the host
-    _, err = output.Export(ctx, path)
-    if err != nil {
-        return err
-    }
+	// write contents of container build/ directory to the host
+	_, err = output.Export(ctx, path)
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
