eaba555d267fc6d4cf1de764b791691265b981a8
Uragami Taichi <backpaper0@gmail.com>
2022-12-08 20:35:10 +0900

    rangeで繰り返し

--- golang/text-template/main.go
+++ golang/text-template/main.go
@@ -9,15 +9,20 @@ import (
 type Data struct {
 	Title string
 	Name string
+	Vars []string
 }
 
 func main() {
 
 	s := `# {{.Title}}
 
-Hello, {{.Name}}!`
+Hello, {{.Name}}!
+
+{{range .Vars}}- {{.}}
+{{end}}
+`
 
 	t := template.Must(template.New("demo").Parse(s))
-	data := Data{Title:"TITLE", Name: "world"}
+	data := Data{Title:"TITLE", Name: "world",Vars:[]string{"foo", "bar", "baz"}}
 	t.Execute(os.Stdout, data)
 }
