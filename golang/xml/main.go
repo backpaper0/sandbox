1c114943a4e5d6ef6c655ea9bc516aa1dcd88d00
Uragami Taichi <backpaper0@gmail.com>
2023-03-29 05:39:45 +0900

    ちょっと修正

--- golang/xml/main.go
+++ golang/xml/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Root struct {
-	Child string `xml:"child"`
+	Child   string `xml:"child"`
 	Child23 string `xml:"child2>child3"`
 }
 
@@ -20,7 +20,9 @@ func main() {
 		</root>
 	`
 
-	r := Root{Child: "", Child23:""}
+	// r := Root{Child: "", Child23:""}
+	var r Root     // わざわざ上記のようにしなくても変数を宣言すればそれで良かった
+	fmt.Println(r) // どんな状態か見てみたいので出力もしておく
 
 	err := xml.Unmarshal([]byte(s), &r)
 	if err != nil {
@@ -29,4 +31,6 @@ func main() {
 
 	fmt.Println(r.Child)
 	fmt.Println(r.Child23)
+
+	fmt.Println(r) // 最後に全部出力しておく
 }
