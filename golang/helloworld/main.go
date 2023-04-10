35d5c7d57715282ea98fa9d2b3b98c82195cdb8d
Uragami Taichi <backpaper0@gmail.com>
2023-04-11 06:04:26 +0900

    メソッドでも試してみた

--- golang/helloworld/main.go
+++ golang/helloworld/main.go
@@ -45,17 +45,19 @@ func (*HelloJa) say() string {
 // **** ポインター ****
 
 func pointerExample() {
+
+	fmt.Print("引数や戻り値のアドレスを確認")
 	s1 := "Hello Pointer"
 	p1 := &s1
 	s2, p2 := pointerExample1(s1)
 	p3 := &s2
 	// p1, p2, p3それぞれアドレスが変わっていることが確認できる。
 	// つまり引数を渡すときや戻り値を返すときに値がコピーされていることがわかる。
-	fmt.Print("引数や戻り値のアドレスを確認")
 	fmt.Printf("p1 = %p\n", p1)
 	fmt.Printf("p2 = %p\n", p2)
 	fmt.Printf("p3 = %p\n", p3)
 
+	fmt.Println("構造体のフィールドで試す")
 	var p4, p5, p6 *string
 	pe1 := PointerExample2{s: "Hello"}
 	p4 = &pe1.s
@@ -63,10 +65,18 @@ func pointerExample() {
 	p6 = &pe2.s
 	// やはりアドレスが変わっていることが確認できる。
 	// 構造体のメンバーも値がコピーされていることがわかる。
-	fmt.Println("構造体のフィールドで試す")
 	fmt.Printf("p4 = %p\n", p4)
 	fmt.Printf("p5 = %p\n", p5)
 	fmt.Printf("p6 = %p\n", p6)
+
+	fmt.Println("メソッドで試す")
+	pe3 := PointerExample2{s: "Hello"}
+	p7 := &pe3
+	pe4, p8 := pe1.pointerExample4()
+	p9 := &pe4
+	fmt.Printf("p7 = %p\n", p7)
+	fmt.Printf("p8 = %p\n", p8)
+	fmt.Printf("p9 = %p\n", p9)
 }
 
 func pointerExample1(s string) (string, *string) {
@@ -79,4 +89,8 @@ type PointerExample2 struct {
 
 func pointerExample3(pe PointerExample2) (PointerExample2, *string) {
 	return pe, &pe.s
+}
+
+func (pe PointerExample2) pointerExample4() (PointerExample2, *PointerExample2) {
+	return pe, &pe
 }
