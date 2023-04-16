37c93c344fa782b27983de88a46f4aa29e65442d
Uragami Taichi <backpaper0@gmail.com>
2023-04-16 21:15:39 +0900

    JavaのtoString()的なやつ

---
+++ golang/sandbox/to_string_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestToString(t *testing.T) {
+	// fmt.Spring などでは String() string があれば呼び出される
+	// 参考 https://pkg.go.dev/fmt#hdr-Printing
+	a := ToStringExample{"foo"}
+	b := fmt.Sprint(a)
+	c := "foofoo"
+	if b != c {
+		t.Errorf("Expected is %[2]v but actual is %[1]v", b, c)
+	}
+}
+
+type ToStringExample struct {
+	Value string
+}
+
+func (a ToStringExample) String() string {
+	return a.Value + a.Value
+}
