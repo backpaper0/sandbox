704eb2590bd75c40ae8ab70f3946370040830dc3
Uragami Taichi <backpaper0@gmail.com>
2023-08-06 10:36:17 +0900

    ToNFADesignを実装

--- automaton/regex.go
+++ automaton/regex.go
@@ -3,6 +3,7 @@ package main
 type Pattern interface {
 	String() string
 	GetPrecedence() int
+	ToNFADesign() *NFADesign
 }
 
 func bracket(pattern Pattern, otherPrecedence int) string {
@@ -83,6 +84,24 @@ func (concatenate *Concatenate) GetPrecedence() int {
 	return 1
 }
 
+func (concatenate *Concatenate) ToNFADesign() *NFADesign {
+	firstNFADesign := concatenate.first.ToNFADesign()
+	secondNFADesign := concatenate.second.ToNFADesign()
+
+	startState := firstNFADesign.startState
+	acceptStates := secondNFADesign.acceptStates
+	rules := make([]*FARule, 0, len(firstNFADesign.rulebook.rules)+len(secondNFADesign.rulebook.rules)+len(firstNFADesign.acceptStates))
+	rules = append(rules, firstNFADesign.rulebook.rules...)
+	rules = append(rules, secondNFADesign.rulebook.rules...)
+	for _, state := range firstNFADesign.acceptStates {
+		rule := NewFARule(state, 0, secondNFADesign.startState)
+		rules = append(rules, rule)
+	}
+	rulebook := NewNFARulebook(rules)
+
+	return NewNFADesign(startState, acceptStates, rulebook)
+}
+
 type Choose struct {
 	first, second Pattern
 }
@@ -102,6 +121,27 @@ func (choose *Choose) GetPrecedence() int {
 	return 0
 }
 
+func (choose *Choose) ToNFADesign() *NFADesign {
+	firstNFADesign := choose.first.ToNFADesign()
+	secondNFADesign := choose.second.ToNFADesign()
+
+	v0 := 0
+	p0 := &v0
+	startState := p0
+	acceptStates := make([]State, 0, len(firstNFADesign.acceptStates)+len(secondNFADesign.acceptStates))
+	acceptStates = append(acceptStates, firstNFADesign.acceptStates...)
+	acceptStates = append(acceptStates, secondNFADesign.acceptStates...)
+
+	rules := make([]*FARule, 0, len(firstNFADesign.rulebook.rules)+len(secondNFADesign.rulebook.rules)+2)
+	rules = append(rules, firstNFADesign.rulebook.rules...)
+	rules = append(rules, secondNFADesign.rulebook.rules...)
+	rules = append(rules, NewFARule(startState, 0, firstNFADesign.startState))
+	rules = append(rules, NewFARule(startState, 0, secondNFADesign.startState))
+	rulebook := NewNFARulebook(rules)
+
+	return NewNFADesign(startState, acceptStates, rulebook)
+}
+
 type Repeat struct {
 	pattern Pattern
 }
@@ -118,4 +158,26 @@ func (repeat *Repeat) String() string {
 
 func (repeat *Repeat) GetPrecedence() int {
 	return 2
+}
+
+func (repeat *Repeat) ToNFADesign() *NFADesign {
+	patternNFADesign := repeat.pattern.ToNFADesign()
+
+	v0 := 0
+	p0 := &v0
+	startState := p0
+	acceptStates := make([]State, 0, len(patternNFADesign.acceptStates)+1)
+	acceptStates = append(acceptStates, patternNFADesign.acceptStates...)
+	acceptStates = append(acceptStates, startState)
+
+	rules := make([]*FARule, 0, len(patternNFADesign.rulebook.rules)+len(patternNFADesign.acceptStates)+1)
+	rules = append(rules, patternNFADesign.rulebook.rules...)
+	for _, acceptState := range patternNFADesign.acceptStates {
+		rule := NewFARule(acceptState, 0, patternNFADesign.startState)
+		rules = append(rules, rule)
+	}
+	rules = append(rules, NewFARule(startState, 0, patternNFADesign.startState))
+	rulebook := NewNFARulebook(rules)
+
+	return NewNFADesign(startState, acceptStates, rulebook)
 }
--- automaton/regexp_test.go
+++ automaton/regexp_test.go
@@ -49,7 +49,98 @@ func TestPatternLiteralToNFADesign(t *testing.T) {
 		{"b", false},
 	}
 	for i, f := range fixtures {
-		t.Run(fmt.Sprintf("TestPatternEmptyToNFADesign%v", i), func(t *testing.T) {
+		t.Run(fmt.Sprintf("TestPatternLiteralToNFADesign%v", i), func(t *testing.T) {
+			accepted := design.Accepts(f.text)
+			if accepted != f.accepted {
+				t.Error()
+			}
+		})
+	}
+}
+
+func TestPatternConcatenateToNFADesign(t *testing.T) {
+	design := NewConcatenate(NewLiteral('a'), NewLiteral('b')).ToNFADesign()
+	fixtures := []struct {
+		text     string
+		accepted bool
+	}{
+		{"a", false},
+		{"ab", true},
+		{"abc", false},
+	}
+	for i, f := range fixtures {
+		t.Run(fmt.Sprintf("TestPatternConcatenateToNFADesign%v", i), func(t *testing.T) {
+			accepted := design.Accepts(f.text)
+			if accepted != f.accepted {
+				t.Error()
+			}
+		})
+	}
+}
+
+func TestPatternChooseToNFADesign(t *testing.T) {
+	design := NewChoose(NewLiteral('a'), NewLiteral('b')).ToNFADesign()
+	fixtures := []struct {
+		text     string
+		accepted bool
+	}{
+		{"a", true},
+		{"b", true},
+		{"c", false},
+	}
+	for i, f := range fixtures {
+		t.Run(fmt.Sprintf("TestPatternChooseToNFADesign%v", i), func(t *testing.T) {
+			accepted := design.Accepts(f.text)
+			if accepted != f.accepted {
+				t.Error()
+			}
+		})
+	}
+}
+
+func TestPatternRepeatToNFADesign(t *testing.T) {
+	design := NewRepeat(NewLiteral('a')).ToNFADesign()
+	fixtures := []struct {
+		text     string
+		accepted bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aaaa", true},
+		{"b", false},
+	}
+	for i, f := range fixtures {
+		t.Run(fmt.Sprintf("TestPatternRepeatToNFADesign%v", i), func(t *testing.T) {
+			accepted := design.Accepts(f.text)
+			if accepted != f.accepted {
+				t.Error()
+			}
+		})
+	}
+}
+
+func TestPatternToNFADesign(t *testing.T) {
+	// (a(|b))*
+	design := NewRepeat(
+		NewConcatenate(
+			NewLiteral('a'),
+			NewChoose(NewEmpty(), NewLiteral('b')),
+		),
+	).ToNFADesign()
+	fixtures := []struct {
+		text     string
+		accepted bool
+	}{
+		{"", true},
+		{"a", true},
+		{"ab", true},
+		{"aba", true},
+		{"abab", true},
+		{"abaab", true},
+		{"abba", false},
+	}
+	for i, f := range fixtures {
+		t.Run(fmt.Sprintf("TestPatternRepeatToNFADesign%v", i), func(t *testing.T) {
 			accepted := design.Accepts(f.text)
 			if accepted != f.accepted {
 				t.Error()
