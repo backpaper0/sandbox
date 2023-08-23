3a06c4b1b4d5ddcf5f6a237562ddf65f68def2c6
Uragami Taichi <backpaper0@gmail.com>
2023-08-24 08:22:32 +0900

    htmxを試してみたい

---
+++ htmx-example/src/main/java/com/example/htmxexample/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.htmxexample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return "message";
+    }
+}
