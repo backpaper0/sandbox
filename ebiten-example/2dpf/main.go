24d7c16f9f547c47170ea61e861a34c8f07d2b8f
Uragami Taichi <backpaper0@gmail.com>
2024-02-12 21:12:19 +0900

    ジャンプしたい

--- ebiten-example/2dpf/main.go
+++ ebiten-example/2dpf/main.go
@@ -6,37 +6,76 @@ import (
 
 	"github.com/backpaper0/sandbox/ebiten-example/assets"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Game struct {
 	ScreenWidth, ScreenHeight int
-	playerX, playerY          int
-	playerMotion              int //0：停止、1：歩き
-	playerDir                 int //-1：左、1：右
-	playerAnimation           int
+	player                    *Player
+}
+
+type Player struct {
+	x, y      int
+	motion    int //0：停止、1：歩き
+	dir       int //-1：左、1：右
+	animation int
+	jump      int //0：地面、1：上昇中、2：下降中
+	vx, vy    int
 }
 
 func (g *Game) Update() error {
-	g.playerMotion = 0
+	p := g.player
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		g.playerMotion = 1
-		g.playerDir = -1
-		g.playerAnimation++
-		g.playerX -= 4
+		p.motion = 1
+		p.dir = -1
+		p.animation++
+		if p.vx > -8 {
+			p.vx -= 1
+		}
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.motion = 1
+		p.dir = 1
+		p.animation++
+		if p.vx < 8 {
+			p.vx += 1
+		}
+	} else {
+		p.motion = 0
+		p.animation = 0
+		p.vx = 0
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		g.playerMotion = 1
-		g.playerDir = 1
-		g.playerAnimation++
-		g.playerX += 4
+	p.x += p.vx
+	if p.x < 0 {
+		p.x = 0
+		p.animation = 0
 	}
-	if g.playerX < 0 {
-		g.playerX = 0
+	if p.x > g.ScreenWidth-16 {
+		p.x = g.ScreenWidth - 16
+		p.animation = 0
 	}
-	if g.playerX > g.ScreenWidth-16 {
-		g.playerX = g.ScreenWidth - 16
+
+	switch p.jump {
+	case 0:
+		if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+			p.jump = 1
+			p.vy = -16
+		}
+	case 1:
+		p.vy += 2 //重力の影響
+		if p.vy > 0 {
+			p.jump = 2
+		}
+	case 2:
+		p.vy += 1 //加速度
 	}
+	p.y += p.vy
+	if p.y >= g.ScreenHeight-32 {
+		p.y = g.ScreenHeight - 32
+		p.vy = 0
+		p.jump = 0
+	}
+
 	return nil
 }
 
@@ -47,14 +86,18 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	op := &ebiten.DrawImageOptions{}
 	idx := 0
-	if g.playerMotion == 1 {
-		idx = g.playerAnimation / 2 % 2
+	p := g.player
+	if p.motion == 1 {
+		idx = p.animation / 2 % 2
 	}
-	if g.playerDir == -1 {
+	if p.jump > 0 {
+		idx = 2
+	}
+	if p.dir == -1 {
 		op.GeoM.Scale(-1, 1)     //左右反転
 		op.GeoM.Translate(16, 0) //反転してズレた分の補正
 	}
-	op.GeoM.Translate(float64(g.playerX), float64(g.playerY))
+	op.GeoM.Translate(float64(p.x), float64(p.y))
 	screen.DrawImage(assets.RoboImgs[idx], op)
 }
 
@@ -63,10 +106,12 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	p := &Player{
+		x: 32, y: 120 - (16 * 2), dir: 1,
+	}
 	g := &Game{
 		ScreenWidth: 320, ScreenHeight: 120,
-		playerX: 32, playerY: 120 - (16 * 2),
-		playerDir: 1,
+		player: p,
 	}
 	ebiten.SetWindowSize(g.ScreenWidth*4, g.ScreenHeight*4)
 	ebiten.SetWindowTitle("2Dプラットフォーマー")
