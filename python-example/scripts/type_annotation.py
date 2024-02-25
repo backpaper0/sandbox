8f00ba8fc0d7dfaff5b3970889aaeb363da69b03
Uragami Taichi <backpaper0@gmail.com>
2024-02-25 21:52:19 +0900

    型アノテーションを学ぶ

---
+++ python-example/scripts/type_annotation.py
@@ -0,0 +1,26 @@
+from typing import Callable, List, TypeVar
+from numbers import Number
+
+def hello(name: str) -> str:
+    return f"Hello, {name}!"
+
+greeting: str = hello("world")
+
+hello_func: Callable[[str], str] = hello
+
+sum: Callable[[int, int], int] = lambda a, b: a + b
+
+def list(xs: List[str]):
+    pass
+
+# PEP 695 generics are not yet supported と言われてしまう。残念
+#
+# T = TypeVar("T")
+# 
+# def f[T](xs: List[T]):
+#     pass
+# 
+# U = TypeVar("U", bound=Number)
+# 
+# def g[U](xs: List[U]):
+#     pass
