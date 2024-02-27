99a5c310bbe499977e8900c82cc769754fe682ee
Uragami Taichi <backpaper0@gmail.com>
2024-02-28 08:33:25 +0900

    DIする

--- hello-fastapi/main.py
+++ hello-fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -20,3 +20,18 @@ def read_item(item_id: int, q: str | None = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+
+async def common_parameters(
+    q: str | None = None, skip: int = 0, limit: int = 100
+):
+    return {"q": q, "skip": skip, "limit": limit}
+
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
