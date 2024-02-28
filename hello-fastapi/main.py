a817f033efda8b9e2581929a7d65758531fb04de
Uragami Taichi <backpaper0@gmail.com>
2024-02-29 05:37:06 +0900

    クラスをDIする

--- hello-fastapi/main.py
+++ hello-fastapi/main.py
@@ -23,15 +23,16 @@ def update_item(item_id: int, item: Item):
 
 
 
-async def common_parameters(
-    q: str | None = None, skip: int = 0, limit: int = 100
-):
-    return {"q": q, "skip": skip, "limit": limit}
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
 @app.get("/items/")
-async def read_items(commons: dict = Depends(common_parameters)):
+async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
     return commons
 
 @app.get("/users/")
-async def read_users(commons: dict = Depends(common_parameters)):
+async def read_users(commons: CommonQueryParams = Depends(CommonQueryParams)):
     return commons
