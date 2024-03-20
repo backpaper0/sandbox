a1ae40bce301a27609774888e7cf18ce2a84a8cc
Uragami Taichi <backpaper0@gmail.com>
2024-03-21 08:06:12 +0900

    メトリクス

--- python-example/scripts/server.py
+++ python-example/scripts/server.py
@@ -3,31 +3,6 @@ from random import randint
 from typing import Union
 import logging
 
-from opentelemetry import trace
-
-tracer = trace.get_tracer("diceroller.tracer")
-
-app = FastAPI()
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-@app.get("/rolldice")
-def roll_dice(player: Union[str, None] = None) -> str:
-    result = str(_roll())
-    if player:
-        logger.warning("%s is rolling the dice: %s", player, result)
-    else:
-        logger.warning("Anonymous player is rolling the dice: %s", result)
-    return result
-
-def _roll() -> int:
-    with tracer.start_as_current_span("roll") as rollspan:
-        res = randint(1, 6)
-        rollspan.set_attribute("roll.value", res)
-        return res
-
-
-
 from opentelemetry import metrics, trace
 from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
@@ -37,6 +12,8 @@ from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+app = FastAPI()
 
 resource = Resource(attributes={
     SERVICE_NAME: "dice-server"
@@ -52,3 +29,30 @@ meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
 metrics.set_meter_provider(meterProvider)
 
 FastAPIInstrumentor.instrument_app(app)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+tracer = trace.get_tracer("diceroller.tracer")
+
+meter = metrics.get_meter("diceroller.meter")
+
+roll_counter = meter.create_counter(
+    "dice.rolls",
+    description="The number of rolls by roll value",
+)
+
+@app.get("/rolldice")
+def roll_dice(player: Union[str, None] = None) -> str:
+    with tracer.start_as_current_span(name="roll") as roll_span:
+        result = str(_roll())
+        roll_span.set_attribute("roll.value", result)
+        roll_counter.add(1, {"roll.value": result})
+        if player:
+            logger.warning("%s is rolling the dice: %s", player, result)
+        else:
+            logger.warning("Anonymous player is rolling the dice: %s", result)
+        return result
+
+def _roll() -> int:
+    return randint(1, 6)
