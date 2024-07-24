ce28245acb090f3bcbe90eed72f707e618fb9480
Uragami Taichi <backpaper0@gmail.com>
2024-07-25 05:00:57 +0900

    戻り値の確認

--- python-example/scripts/asyncio/example1.py
+++ python-example/scripts/asyncio/example1.py
@@ -2,10 +2,21 @@ import asyncio
 import time
 
 
+async def say(sleep: float, msg: str) -> str:
+    await asyncio.sleep(sleep)
+    return f"{time.strftime('%X')} {msg}"
+
+
 async def main() -> None:
-    print(f"{time.strftime('%X')} hello")
-    await asyncio.sleep(1)
-    print(f"{time.strftime('%X')} world")
+    print(f"{time.strftime('%X')} start")
+
+    said1 = await say(1, "hello")
+    print(said1)
+
+    said2 = await say(1, "world")
+    print(said2)
+
+    print(f"{time.strftime('%X')} end")
 
 
 asyncio.run(main())
