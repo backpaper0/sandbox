a3a427a0bf668f75b87b4b725528f07e4540e6ab
Uragami Taichi <backpaper0@gmail.com>
2024-08-18 08:10:15 +0900

    PickleでBase64した方がサイズは小さそう

--- python-example/scripts/struct_example.py
+++ python-example/scripts/struct_example.py
@@ -2,6 +2,12 @@
 list[float]をなるべく小さなサイズでシリアライズする方法を検証するスクリプト。
 
 ```
+poetry run python -m scripts.struct_example
+```
+
+あるいは
+
+```
 python -m scripts.struct_example
 ```
 
@@ -10,6 +16,7 @@ structモジュールを使ったpackはサイズが一番小さくなるが、u
 pickleはstructよりもサイズが大きいが、unpackしても誤差が生じない。
 """
 
+import base64
 import json
 import pickle
 import struct
@@ -32,20 +39,29 @@ b2.write(struct.pack(f"{len(data)}f", *data))
 
 pickle.dump(data, b3)
 
-print(f"  Text file size: {len(b1.getvalue()):,}")
-print(f"Binary file size: {len(b2.getvalue()):,}")
-print(f"Pickle file size: {len(b3.getvalue()):,}")
+print(f"            Text size: {len(b1.getvalue()):,}")
+print(f"          Binary size: {len(b2.getvalue()):,}")
+print(f"          Pickle size: {len(b3.getvalue()):,}")
+
+print()
+
+c1 = b1.getvalue().decode()
+c3 = base64.b64encode(b3.getvalue()).decode()
+print(f"            Text: {c1[:100]}...")
+print(f"Pickle w/ Base64: {c3[:100]}...")
+print(f"            Text char count: {len(c1):,}")
+print(f"Pickle w/ Base64 char count: {len(c3):,}")
 
 print()
 
 size = 5
-print(f"Original data: {data[:size]}")
+print(f"Original: {data[:size]}")
 
 data1 = json.loads(b1.getvalue())
-print(f"    Text file: {data1[:size]}")
+print(f"    Text: {data1[:size]}")
 
 data2 = list(struct.unpack(f"{len(data)}f", b2.getbuffer()))
-print(f"  Binary file: {data2[:size]}")
+print(f"  Binary: {data2[:size]}")
 
 data3 = pickle.loads(b3.getbuffer())
-print(f"  Pickle file: {data3[:size]}")
+print(f"  Pickle: {data3[:size]}")
