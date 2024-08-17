cf893bafe03dc4c8193d8868c9121c63ed673bfc
Uragami Taichi <backpaper0@gmail.com>
2024-08-18 07:48:19 +0900

    AOAIへの対応

--- python-embedding/app/embeddings_client.py
+++ python-embedding/app/embeddings_client.py
@@ -5,7 +5,7 @@
 import os
 from typing import Awaitable, Callable
 
-from openai import AsyncOpenAI
+from openai import AsyncAzureOpenAI, AsyncOpenAI
 
 EmbeddingsClient = Callable[[str], Awaitable[list[float]]]
 
@@ -18,8 +18,16 @@ def get_embeddings_client() -> EmbeddingsClient:
         EmbeddingsClient: 埋め込みクライアントを返します。
     """
 
-    client = AsyncOpenAI()
-    embeddings_model = os.environ["EMBEDDINGS_MODEL"]
+    use_azure = os.getenv("AZURE_OPENAI_ENDPOINT") is not None
+
+    if use_azure:
+        print("Azure OpenAIを使用")
+
+    client = AsyncAzureOpenAI() if use_azure else AsyncOpenAI()
+
+    embeddings_model = os.environ[
+        "EMBEDDINGS_DEPLOYMENT_NAME" if use_azure else "EMBEDDINGS_MODEL"
+    ]
 
     async def _f(text: str) -> list[float]:
         resp = await client.embeddings.create(input=text, model=embeddings_model)
