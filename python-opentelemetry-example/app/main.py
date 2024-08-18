58d0948cf22ce3a59722e3650b1f4f13aa6d88eb
Uragami Taichi <backpaper0@gmail.com>
2024-08-18 16:22:27 +0900

    設定の切り出し

--- python-opentelemetry-example/app/main.py
+++ python-opentelemetry-example/app/main.py
@@ -1,23 +1,31 @@
 from base64 import b64encode
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.semconv.resource import ResourceAttributes
+
+load_dotenv(".env.local")
 
 app = FastAPI()
 
-provider = TracerProvider()
+resource = Resource.create({ResourceAttributes.SERVICE_NAME: "telemetry-example"})
+
+provider = TracerProvider(resource=resource)
 trace.set_tracer_provider(provider)
 
-exporter1 = OTLPSpanExporter(
-    endpoint="http://localhost:5080/api/default/v1/traces",
-    headers={
-        "Authorization": f"Basic {b64encode(b'root@example.com:pass1234').decode()}"
-    },
-)
+# exporter1 = OTLPSpanExporter(
+#     endpoint="http://localhost:5080/api/default/v1/traces",
+#     headers={
+#         "Authorization": f"Basic {b64encode(b'root@example.com:pass1234').decode()}"
+#     },
+# )
+exporter1 = OTLPSpanExporter()
 processor1 = BatchSpanProcessor(exporter1)
 provider.add_span_processor(processor1)
 
