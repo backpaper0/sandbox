f0c3275ee607bcaf1e528318d3029790667ba0e0
Uragami Taichi <backpaper0@gmail.com>
2024-09-23 13:05:45 +0900

    ruffによるチェック対応

--- python-example/scripts/type_annotation.py
+++ python-example/scripts/type_annotation.py
@@ -1,8 +1,9 @@
-from typing import Callable, List, TypeVar
-from numbers import Number
+from typing import Callable, List
+
 
 def hello(name: str) -> str:
     return f"Hello, {name}!"
+
 
 greeting: str = hello("world")
 
@@ -10,17 +11,19 @@ hello_func: Callable[[str], str] = hello
 
 sum: Callable[[int, int], int] = lambda a, b: a + b
 
+
 def list(xs: List[str]):
     pass
+
 
 # PEP 695 generics are not yet supported と言われてしまう。残念
 #
 # T = TypeVar("T")
-# 
+#
 # def f[T](xs: List[T]):
 #     pass
-# 
+#
 # U = TypeVar("U", bound=Number)
-# 
+#
 # def g[U](xs: List[U]):
 #     pass
