f6d69bd5b30f3cef3afcf2da192a860139512af1
Uragami Taichi <backpaper0@gmail.com>
2024-12-22 06:05:14 +0900

    新しいCosmos DBエミュレータを試す

--- python-cosmosdb-example/app/__init__.py
+++ python-cosmosdb-example/app/__init__.py
@@ -1,17 +1,17 @@
 import os
 
 from dotenv import load_dotenv
-from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.semconv.resource import ResourceAttributes
 
 load_dotenv()
 
-if os.getenv("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT") is not None:
+if os.getenv("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT", ""):
+    from opentelemetry import trace
+    from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+    from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
+    from opentelemetry.sdk.resources import Resource
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import BatchSpanProcessor
+    from opentelemetry.semconv.resource import ResourceAttributes
     resource = Resource.create(
         {ResourceAttributes.SERVICE_NAME: "python-cosmosdb-example"}
     )
