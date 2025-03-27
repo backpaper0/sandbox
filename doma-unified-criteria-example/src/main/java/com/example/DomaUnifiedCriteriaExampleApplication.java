aeadff42bb7fd3db84d64d4b20655af0b71e7351
Uragami Taichi <backpaper0@gmail.com>
2025-03-27 21:57:32 +0900

    pagination

--- doma-unified-criteria-example/src/main/java/com/example/DomaUnifiedCriteriaExampleApplication.java
+++ doma-unified-criteria-example/src/main/java/com/example/DomaUnifiedCriteriaExampleApplication.java
@@ -1,13 +1,23 @@
 package com.example;
 
 import java.util.List;
+import java.util.function.Consumer;
 
+import org.seasar.doma.boot.Pageables;
+import org.seasar.doma.jdbc.SelectOptions;
+import org.seasar.doma.jdbc.SelectOptionsAccessor;
 import org.seasar.doma.jdbc.criteria.QueryDsl;
+import org.seasar.doma.jdbc.criteria.declaration.WhereDeclaration;
+import org.seasar.doma.jdbc.criteria.expression.Expressions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
 public class DomaUnifiedCriteriaExampleApplication implements ApplicationRunner {
@@ -21,16 +31,31 @@ public class DomaUnifiedCriteriaExampleApplication implements ApplicationRunner 
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Pageable pageable = PageRequest.of(2, 4);
+
+        SelectOptions selectOptions = Pageables.toSelectOptions(pageable);
+        int offset = (int) SelectOptionsAccessor.getOffset(selectOptions);
+        int limit = (int) SelectOptionsAccessor.getLimit(selectOptions);
+
         Animal_ animals_ = new Animal_();
+
+        Consumer<WhereDeclaration> conditions = q -> q.eq(animals_.species, "コグマ");
+
         List<Animal> animals = queryDsl.from(animals_)
-            .where(where -> where.eq(animals_.species, "コグマ"))
-            .orderBy(orderBy -> orderBy.asc(animals_.id))
-            .offset(8)
-            .limit(3)
+            .where(conditions)
+            .orderBy(q -> q.asc(animals_.id))
+            .offset(offset)
+            .limit(limit)
             .fetch();
-        for (Animal animal : animals) {
-            System.out.println(animal.name());
-        }
+
+        long total = queryDsl.from(animals_).where(conditions).select(Expressions.count()).fetchOne();
+
+        Page<Animal> page = new PageImpl<>(animals, pageable, total);
+        page.forEach(animal -> System.out.println(animal.name()));
+        // のりお
+        // パッチ
+        // パンタ
+        // ピッコロ
     }
 
 }
