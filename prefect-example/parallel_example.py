4e7f9e9b823fe4a8b120db90e48872cbdb0e0f89
Uragami Taichi <backpaper0@gmail.com>
2025-05-03 14:21:11 +0900

    タスクは別スレッドで起動するっぽい。submitメソッドだとそうなるのかな？

--- prefect-example/parallel_example.py
+++ prefect-example/parallel_example.py
@@ -1,9 +1,16 @@
 from prefect import flow, task
 import time
+import threading
+import os
+
+
+def _f(name: str) -> None:
+    print(f"PID:{os.getpid()} ThreadID:{threading.get_ident()} name:{name}")
 
 
 @task
 def hello() -> None:
+    _f("hello")
     time.sleep(1)
     print("Hello")
     time.sleep(1)
@@ -11,6 +18,7 @@ def hello() -> None:
 
 @task
 def world() -> None:
+    _f("world")
     time.sleep(1)
     print("World")
     time.sleep(1)
@@ -18,11 +26,13 @@ def world() -> None:
 
 @task
 def sleep(seconds: int) -> None:
+    _f("sleep")
     time.sleep(seconds)
 
 
 @flow
 def parallel_example() -> None:
+    _f("parallel_example")
     h = hello.submit()
     w = world.submit()
     s = sleep.map([1, 2, 3], wait_for=[h, w])
@@ -30,4 +40,5 @@ def parallel_example() -> None:
 
 
 if __name__ == "__main__":
+    _f("main")
     parallel_example()
